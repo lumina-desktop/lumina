2c79ec7672c886349b2a766eccc145bcb93a34e6
Ken Moore <ken@pcbsd.org>
2014-09-12 12:25:00 -0400

    Update lumina-config to use the new OS prefix settings.

--- lumina-config/mainUI.cpp
+++ lumina-config/mainUI.cpp
@@ -7,6 +7,8 @@
 #include "mainUI.h"
 #include "ui_mainUI.h" //the designer *.ui file
 
+#include <LuminaOS.h>
+
 MainUI::MainUI() : QMainWindow(), ui(new Ui::MainUI()){
   ui->setupUi(this); //load the designer file
   this->setWindowIcon( LXDG::findIcon("preferences-desktop-display","") );
@@ -224,12 +226,7 @@ void MainUI::setupMenus(){
   ui->combo_session_wloc->addItem( tr("Cascade"), "CascadePlacement");
   ui->combo_session_wloc->addItem( tr("Underneath Mouse"), "UnderMousePlacement");
   ui->combo_session_wtheme->clear();
-  #if defined(__FreeBSD__) || defined(__DragonFly__)
-  QDir fbdir("/usr/local/share/fluxbox/styles");
-  #endif
-  #ifdef __linux__
-  QDir fbdir("/usr/share/fluxbox/styles");
-  #endif
+  QDir fbdir(LOS::AppPrefix()+"share/fluxbox/styles");
   QStringList fbstyles = fbdir.entryList(QDir::Dirs | QDir::NoDotAndDotDot, QDir::Name);
   for(int i=0; i<fbstyles.length(); i++){
     ui->combo_session_wtheme->addItem(fbstyles[i], fbdir.absoluteFilePath(fbstyles[i]));
