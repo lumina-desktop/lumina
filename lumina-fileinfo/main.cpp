7f986188ec65509d3b99e3b4bbf996258ccae683
wi <william.os4y@gmail.com>
2015-04-26 14:01:23 +0200

    since mixing tabs and spaces can generate troubles in some editors, I've removed the tabs

--- lumina-fileinfo/main.cpp
+++ lumina-fileinfo/main.cpp
@@ -9,22 +9,22 @@
 
 int main(int argc, char ** argv)
 {
-    QApplication a(argc, argv);
-    LUtils::LoadTranslation(&a, "lumina-fileinfo");
-    LuminaThemeEngine theme(&a);
+  QApplication a(argc, argv);
+  LUtils::LoadTranslation(&a, "lumina-fileinfo");
+  LuminaThemeEngine theme(&a);
 
-    Dialog w;
-    QObject::connect(&theme, SIGNAL(updateIcons()), &w, SLOT(setupIcons()) );
-    if (argc==2) {
-        w.LoadDesktopFile(QString(argv[1]).simplified());
-    } else if (argc==3) {
-        w.Initialise(QString(argv[1]).simplified());
-        w.LoadDesktopFile(QString(argv[2]).simplified());
-    } else {
-        w.MissingInputs();
-    }
-    w.show();
+  Dialog w;
+  QObject::connect(&theme, SIGNAL(updateIcons()), &w, SLOT(setupIcons()) );
+  if (argc==2) {
+    w.LoadDesktopFile(QString(argv[1]).simplified());
+  } else if (argc==3) {
+    w.Initialise(QString(argv[1]).simplified());
+    w.LoadDesktopFile(QString(argv[2]).simplified());
+  } else {
+    w.MissingInputs();
+  }
+  w.show();
 
-    int retCode = a.exec();
-    return retCode;
+  int retCode = a.exec();
+  return retCode;
 }
