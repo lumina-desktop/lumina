fddbfcf43962df9571993b0b2b937eb800a877e1
wi <william.os4y@gmail.com>
2015-04-26 11:29:44 +0200

    cleanup tabs in MainUI.h

--- lumina-search/MainUI.h
+++ lumina-search/MainUI.h
@@ -10,7 +10,6 @@
 #include <QMainWindow>
 #include <QString>
 #include <QTimer>
-//#include <QSettings>
 #include <QThread>
 #include <QProcess>
 #include <QListWidgetItem>
@@ -21,49 +20,49 @@
 #include "Worker.h"
 
 namespace Ui{
-	class MainUI;
+  class MainUI;
 };
 
 class MainUI : public QMainWindow{
-	Q_OBJECT
+  Q_OBJECT
 public:
-	MainUI();
-	~MainUI();
-	QJsonObject jsonObject;
+  MainUI();
+  ~MainUI();
+  QJsonObject jsonObject;
 
 public slots:
-	void setupIcons();
-	bool initialise(QString param, QString paramValue);
-	bool initialise(QString param, QString paramValue, QString searchString);
+  void setupIcons();
+  bool initialise(QString param, QString paramValue);
+  bool initialise(QString param, QString paramValue, QString searchString);
 
 private:
-	Ui::MainUI *ui;
-	QThread *workthread;
-	QTimer *livetime;
-	Worker *searcher;
-	//QSettings *settings;
+  Ui::MainUI *ui;
+  QThread *workthread;
+  QTimer *livetime;
+  Worker *searcher;
+  //QSettings *settings;
 
 private slots:
-	//Button Slots
-	void closeApplication(){
-	  this->close();
-	}
-	void LaunchItem();
-	void LaunchItem(QListWidgetItem*);
-	void searchTypeChanged();
-	void configureSearch();
-	
-	void searchChanged(); //for active searching while typing
-	
-	//Worker Interaction
-	void startSearch(); //emit the proper signal for the worker
-	void foundSearchItem(QString path); //To get the worker's results
-	void stopSearch();
-	void searchMessage(QString);
-	void searchFinished();
-	
+  //Button Slots
+  void closeApplication(){
+    this->close();
+  }
+  void LaunchItem();
+  void LaunchItem(QListWidgetItem*);
+  void searchTypeChanged();
+  void configureSearch();
+
+  void searchChanged(); //for active searching while typing
+
+  //Worker Interaction
+  void startSearch(); //emit the proper signal for the worker
+  void foundSearchItem(QString path); //To get the worker's results
+  void stopSearch();
+  void searchMessage(QString);
+  void searchFinished();
+
 signals:
-	void SearchTerm(QString term, bool isApp); //to connect to the worker's slot
+  void SearchTerm(QString term, bool isApp); //to connect to the worker's slot
 };
 
 #endif
