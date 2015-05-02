cd39ba63195c78f4d990bbebdc5869000dba87c9
kloun <andrey0bolkonsky@gmail.com>
2015-05-02 16:46:42 +0300

    translated lumina-info on ru (русский)

--- lumina-info/i18n/lumina-info_ru.ts
+++ lumina-info/i18n/lumina-info_ru.ts
@@ -6,87 +6,87 @@
     <message>
         <location filename="../MainUI.ui" line="14"/>
         <source>Lumina Desktop Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация о рабочем окружении Lumina</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="59"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Основное</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="77"/>
         <source>Desktop Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия окружения</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="97"/>
         <source>OS Build:</source>
-        <translation type="unfinished"></translation>
+        <translation>Сборка ОС:</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="120"/>
         <source>Qt Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия Qt:</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="133"/>
         <source>Source Repository:</source>
-        <translation type="unfinished"></translation>
+        <translation>Репозиторий с исходным ходом:</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="166"/>
         <source>Documentation:</source>
-        <translation type="unfinished"></translation>
+        <translation>Документация:</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="173"/>
         <source>&lt;a href=http://wiki.pcbsd.org/index.php/Lumina/10.1.1&quot;&gt;Open in web browser&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;a href=http://wkii.pcbsd.org/index.php/Lumina/10.1.1&quot;&gt;Открыть в веб-браузере&lt;/a</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="191"/>
         <source>View Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Просмотр информации</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="214"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Лицензия</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="231"/>
         <source>Acknowledgements</source>
-        <translation type="unfinished"></translation>
+        <translation>Благодарности</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="249"/>
         <source>Project Lead:</source>
-        <translation type="unfinished"></translation>
+        <translation>Лидер проекта:</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="256"/>
         <source>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="269"/>
         <source>Contributors:</source>
-        <translation type="unfinished"></translation>
+        <translation>Участникм проекта:</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="282"/>
         <source>Sponsors:</source>
-        <translation type="unfinished"></translation>
+        <translation>Спонсоры:</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="344"/>
         <source>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Open in web browser&lt;a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Открыть в веб-браузере&lt;a&gt;</tranрslation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="370"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрыть</translation>
     </message>
 </context>
 </TS>
