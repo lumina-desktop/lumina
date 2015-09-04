109b5d1f0227c0d20664597a4e529547b757677b
Ken Moore <moorekou@gmail.com>
2015-09-04 09:27:58 -0400

    Clean out all the old XLib functions which were commented out, and start adding new functions for the window manager (direct access/control - not requests).

--- libLumina/LuminaX11.h
+++ libLumina/LuminaX11.h
@@ -45,69 +45,6 @@
 
 #define URGENCYHINT (1L << 8) //For window urgency detection
 
-/*class LX11{
-public:
-	enum WINDOWSTATE {VISIBLE, INVISIBLE, ACTIVE, ATTENTION, IGNORE};
-	
-	//Internal Use Functions
-	//static xcb_ewmh_connection_t* EWMH_C(); //Get the XCB_ewmh handle;
-	
-	//General Info Functions
-	static QList<WId> WindowList(); //List all current windows
-	static QList<WId> GetClientList(); // _NET_WM_CLIENT list
-	static QList<WId> GetClientStackingList(); // _NET_WM_CLIENT_STACKING list
-	static QList<WId> findChildren(WId, int); //Recursive utility for getting all children of a window
-	static WId ActiveWindow();				//List the currently-active window
-	
-	//Window Manager Interaction
-	static void SetNumberOfDesktops(int); 	// set number of virtual desktops
-	static void SetCurrentDesktop(int); 		// set current virtual desktop
-	static int GetNumberOfDesktops(); 		// get number of virtual desktops
-	static int GetCurrentDesktop();			// get current virtual desktop
-	//static bool ValidWindowEvent(Atom evAtom);	//Check if the property changed is a window event
-	
-	//Single Window Management
-	static void CloseWindow(WId);			// Close the Window
-	static void KillWindow(WId);			// Kill the window/application
-	static void IconifyWindow(WId);			// Iconify (minimize) the Window
-	static void RestoreWindow(WId);		// Restore (non-iconify) the Window
-	static void ActivateWindow(WId);		// Make the window active;
-	static void ReservePanelLocation(WId win, int xstart, int ystart, int width, int height, QString loc); //loc=[top,bottom,left,right]
-	static void SetAsSticky(WId win);
-	static void SetAsPanel(WId win);
-	static void SetAsDesktop(WId win);
-
-	//Advanced Window methods
-	static void MoveResizeWindow(WId, QRect);
-	static void ResizeWindow(WId, int width, int height);
-	static WId CreateWindow(WId parent, QRect);
-	static void DestroyWindow(WId);
-	static bool EmbedWindow(WId win, WId container);
-	static bool UnembedWindow(WId win);
-	
-	//Single Window Information
-	static QString WindowClass(WId);		// Class name  (used for ID purposes)
-	static QString WindowName(WId); 		// long name (untranslated)
-	static QString WindowVisibleName(WId); 	// long name (translated)
-	static QString WindowIconName(WId); 	// short name (untranslated)
-	static QString WindowVisibleIconName(WId); // short name (translated)
-	//static QIcon WindowIcon(WId);			// Icon for the window
-	//static QPixmap WindowImage(WId win, bool useleader=true);		// Image for the window
-	static int WindowDesktop(WId);			// Which virtual desktop the window is on
-	static WINDOWSTATE GetWindowState(WId win); //State of activity
-	static WId leaderWindow(WId); 			//Get the main window if this one is a redirect
-	static bool isNormalWindow(WId win, bool includeDialogs=false);
-	
-	//System Tray Management
-	static WId startSystemTray(int screen = 0); //Startup the system tray
-	static void closeSystemTray(WId); //Close the system tray
-	static QList<WId> findOrphanTrayWindows(); //scan for any orphaned tray apps
-	
-	
-	//EWMH Convenience functions
-	static QString getNetWMProp(WId win, QString prop); //Returns a _NET_WM_* string value
-};*/
-
 //XCB Library replacement for LX11 (Qt5 uses XCB instead of XLib)
 class LXCB{
 	
@@ -175,6 +112,12 @@ public:
 	//System Tray Management
 	WId startSystemTray(int screen = 0); //Startup the system tray (returns window ID for tray)
 	void closeSystemTray(WId); //Close the system tray
+	
+	//============
+	// WM Functions (directly changing properties/settings)
+	//  - Using these directly may prevent the WM from seeing the change
+	//============
+	void WM_CloseWindow(WId win);
 	
 };
 
