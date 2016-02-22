9cc9b047ff5f4ac643810fb56f7dc4b5b6f9065a
Zyixc <matthijsnieuwboer@gmail.com>
2016-02-22 14:52:10 +0100

    Add dutch translation for i18n/lumina-fm

--- lumina-fm/i18n/lumina-fm_nl.ts
+++ lumina-fm/i18n/lumina-fm_nl.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../MainUI.ui" line="51"/>
         <source>Open Multimedia Player</source>
-        <translation type="unfinished"></translation>
+        <translation>Open multimedia speler</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="54"/>
@@ -31,23 +31,23 @@
     <message>
         <location filename="../MainUI.ui" line="64"/>
         <source>View Slideshow</source>
-        <translation type="unfinished"></translation>
+        <translation>Bekijk diavoorstelling</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="77"/>
         <source>Restore File(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstel bestand(en)</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="106"/>
         <source>Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Afspeellijst</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="116"/>
         <location filename="../MainUI.ui" line="268"/>
         <source>Go to Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Ga naar volgende</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="127"/>
@@ -57,82 +57,82 @@
     <message>
         <location filename="../MainUI.ui" line="212"/>
         <source>Go to Beginning</source>
-        <translation type="unfinished"></translation>
+        <translation>Ga naar begin</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="222"/>
         <source>Go to Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Ga naar vorige</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="278"/>
         <source>Go to End</source>
-        <translation type="unfinished"></translation>
+        <translation>Ga naar einde</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="356"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Naam</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="361"/>
         <source>Date Modified</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum aangepast</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="366"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Grootte</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="371"/>
         <source>Owner</source>
-        <translation type="unfinished"></translation>
+        <translation>Eigenaar</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="381"/>
         <source>Restore entire directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstel hele map</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="384"/>
         <source>Restore All</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstel alles</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="394"/>
         <source>Restore Selected Item</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstel geselecteerd items</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="397"/>
         <source>Restore Selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstel geselecteerde</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="420"/>
         <source>Overwrite Existing Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Overschrijf bestaande bestanden</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="443"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestand</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="451"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Bewerken</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="457"/>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Bekijken</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="465"/>
         <source>Bookmarks</source>
-        <translation type="unfinished"></translation>
+        <translation>Bladwijzers</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="476"/>
@@ -142,12 +142,12 @@
     <message>
         <location filename="../MainUI.ui" line="497"/>
         <source>New &amp;Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuwe &amp;tab</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="502"/>
         <source>Close Tab</source>
-        <translation type="unfinished"></translation>
+        <translation>Tab sluiten</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="507"/>
@@ -157,67 +157,67 @@
     <message>
         <location filename="../MainUI.ui" line="512"/>
         <source>&amp;Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Voorkeuren</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="523"/>
         <source>Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>Snelkoppelingen</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="534"/>
         <source>Music Player</source>
-        <translation type="unfinished"></translation>
+        <translation>Muziek speler</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="545"/>
         <source>Image Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Afbeelding viewer</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="550"/>
         <source>UpDir</source>
-        <translation type="unfinished"></translation>
+        <translation>Map omhoog</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="553"/>
         <source>Go up one directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Ga een map omhoog</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="558"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoofd map</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="561"/>
         <source>Go to your home directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Ga naar uw hoofd map</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="572"/>
         <source>View Hidden Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Bekijk verborgen bestanden</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="577"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Terug</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="580"/>
         <source>Back to directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Terug naar map</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="585"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Verversen</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="588"/>
         <source>Refresh Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Ververs map</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="591"/>
@@ -227,12 +227,12 @@
     <message>
         <location filename="../MainUI.ui" line="596"/>
         <source>Bookmark</source>
-        <translation type="unfinished"></translation>
+        <translation>Bladwijzer</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="599"/>
         <source>Bookmark this directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Refereer deze map</translation>
     </message>
 </context>
 </TS>
