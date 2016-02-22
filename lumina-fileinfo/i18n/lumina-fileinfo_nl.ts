0e22d697212823b7f87831c7593c8a559cc78a08
Zyixc <matthijsnieuwboer@gmail.com>
2016-02-22 14:29:52 +0100

    Add dutch translation for i18n/lumina-fileinfo

--- lumina-fileinfo/i18n/lumina-fileinfo_nl.ts
+++ lumina-fileinfo/i18n/lumina-fileinfo_nl.ts
@@ -6,74 +6,74 @@
     <message>
         <location filename="../dialog.ui" line="14"/>
         <source>File Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestand informatie</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="22"/>
         <source>Working dir:</source>
-        <translation type="unfinished"></translation>
+        <translation>Werkmap:</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="56"/>
         <source>Use startup notification</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik opstart notificatie</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="70"/>
         <source>Icon:</source>
-        <translation type="unfinished"></translation>
+        <translation>Icoon:</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="77"/>
         <source>Command:</source>
-        <translation type="unfinished"></translation>
+        <translation>Commando:</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="84"/>
         <source>Comment:</source>
-        <translation type="unfinished"></translation>
+        <translation>Commentaar:</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="118"/>
         <source>Run in terminal</source>
-        <translation type="unfinished"></translation>
+        <translation>Draaien in terminal</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="125"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Naam</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="132"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opties</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="162"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annuleren</translation>
     </message>
     <message>
         <location filename="../dialog.ui" line="169"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Toepassen</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="84"/>
         <location filename="../dialog.cpp" line="125"/>
         <location filename="../dialog.cpp" line="160"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="84"/>
         <source>Lumina-fileinfo requires inputs:</source>
-        <translation type="unfinished"></translation>
+        <translation>Lumina-bestandinformatie vereist inputs:</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="84"/>
         <source>Example: &quot;%1&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Voorbeeld:</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="101"/>
@@ -83,83 +83,84 @@
     <message>
         <location filename="../dialog.cpp" line="111"/>
         <source>Working dir</source>
-        <translation type="unfinished"></translation>
+        <translation>Werkmap</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="125"/>
         <source>The filename cannot start with a &quot;-&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestandnaam kan niet beginnen met een &quot;-&quat;.</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="160"/>
         <source>Problem to read the desktop file called:</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout tijdens lezen van bureaublad bestand genaamd:</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="182"/>
         <source>There are some issues with this file !!!!</source>
-        <translation type="unfinished"></translation>
+        <translation>Er zijn problemen met dit bestand !!!!</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="183"/>
         <source>Either you correct this file your self with an editor, or you start from scratch using the link or app template.
 Please note that this process will update the file called:</source>
-        <translation type="unfinished"></translation>
+        <translation>U kunt dit bestand zowel zelf aanpassen met een editor, of opnieuw beginnen vanaf scratch met het gebruik van de link of de applicatie blouwdruk.
+Opmerking: dit process zal de file aanpassen genaamd:</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="217"/>
         <location filename="../dialog.cpp" line="323"/>
         <source>Open command</source>
-        <translation type="unfinished"></translation>
+        <translation>Open commando</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="217"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Alle bestanden (*)</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="233"/>
         <source>Working Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Werkmap</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="285"/>
         <location filename="../dialog.cpp" line="300"/>
         <location filename="../dialog.cpp" line="309"/>
         <source>Problem to write to disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout tijdens schrijven naar schijf</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="285"/>
         <source>We have a problem to write the adapted desktop file to the disk. Can you re-try the modification after solving the issue with the disk ?</source>
-        <translation type="unfinished"></translation>
+        <translation>Er is een probleem bij het schrijven van het aangepast bureaublad bestand naar de schijf. U kunt het opnieuw proberen na het verhelpen van het probleem met de schijf?</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="300"/>
         <location filename="../dialog.cpp" line="309"/>
         <source>We have a problem to execute the following command:</source>
-        <translation type="unfinished"></translation>
+        <translation>Er is een probleem bij het uitvoeren van het volgende commando:</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="323"/>
         <source>Image Files (*.png *.jpg *.bmp)</source>
-        <translation type="unfinished"></translation>
+        <translation>Afbeelding bestanden (*.png *.jpg *.bmp)</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="334"/>
         <location filename="../dialog.cpp" line="351"/>
         <source>By modifying this value, you will loose all translated versions</source>
-        <translation type="unfinished"></translation>
+        <translation>Bij het toevoegen van deze waarde zullen alle vertalingen hiervan verloren gaan</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="335"/>
         <source>The field: Name is translated in several other languages. If you want to continue, you will loose all translated versions</source>
-        <translation type="unfinished"></translation>
+        <translation>Dit veld: Naam is vertaald in verschillende andere talen. Als u wilt doorgaan zullen alle vertalingen hiervan verloren gaan</translation>
     </message>
     <message>
         <location filename="../dialog.cpp" line="352"/>
         <source>The field: Comment is translated in several other languages. If you want to continue, you will loose all translated versions</source>
-        <translation type="unfinished"></translation>
+        <translation>Dit veld: Commentaar is vertaald in verschillende andere talen. Als u wilt doorgaan zullen alle vertalingen hiervan verloren gaan</translation>
     </message>
 </context>
 </TS>
