87128040a972be55f846e3ab6e803187ebe2501d
Ken Moore <moorekou@gmail.com>
2016-04-26 10:02:56 -0400

    Add in all the translation files for lumina-terminal

---
+++ src-qt5/desktop-utils/lumina-terminal/i18n/lumina-terminal_fr.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="../TrayIcon.cpp" line="123"/>
+        <source>Trigger Terminal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TrayIcon.cpp" line="125"/>
+        <source>Top of Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TrayIcon.cpp" line="130"/>
+        <source>Close Terminal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TrayIcon.cpp" line="139"/>
+        <source>Move To Monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../TrayIcon.cpp" line="142"/>
+        <source>Monitor %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
