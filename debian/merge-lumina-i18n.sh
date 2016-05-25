34e77cf73cd0441309192fdca3123d61df0b909b
Christopher Roy Bratusek <nano@jpberlin.de>
2016-05-25 21:23:29 +0200

    update merge-lumina-i18n.sh

--- debian/merge-lumina-i18n.sh
+++ debian/merge-lumina-i18n.sh
@@ -10,15 +10,22 @@ git clone https://github.com/pcbsd/lumina-i18n.git
 LANGS=("${LNGDIR}"/*)
 LANGS=(${LANGS[@]/*\/ts\/})
 
-UTILS=( config desktop fileinfo fm info open screenshot search xconfig )
-
-echo ${LANGS[@]}
+UTILS=( config desktop fileinfo fm info open screenshot search textedit xconfig )
 
 for lng in ${LANGS[@]}; do
 	for utl in ${UTILS[@]}; do
-		[[ -e "${LNGDIR}"/${lng}/lumina-${utl}.ts ]] && \
-			cp "${LNGDIR}"/${lng}/lumina-${utl}.ts \
-				"${CURDIR}"/lumina-${utl}/i18n/lumina-${utl}_${lng}.ts
+		if [[ -e "${LNGDIR}"/${lng}/lumina-${utl}.ts ]]; then
+			if [[ -d "${CURDIR}"/src-qt5/core/lumina-${utl} ]]; then
+				cp "${LNGDIR}"/${lng}/lumina-${utl}.ts \
+					"${CURDIR}"/src-qt5/core/lumina-${utl}/i18n/lumina-${utl}_${lng}.ts
+			elif [[ -d "${CURDIR}"/src-qt5/core-utils/lumina-${utl} ]]; then
+				cp "${LNGDIR}"/${lng}/lumina-${utl}.ts \
+					"${CURDIR}"/src-qt5/core-utils/lumina-${utl}/i18n/lumina-${utl}_${lng}.ts
+			elif [[ -d "${CURDIR}"/src-qt5/desktop-utils/lumina-${utl} ]]; then
+				cp "${LNGDIR}"/${lng}/lumina-${utl}.ts \
+					"${CURDIR}"/src-qt5/desktop-utils/lumina-${utl}/i18n/lumina-${utl}_${lng}.ts
+			fi
+		fi
 	done
 done
 
@@ -28,6 +35,7 @@ echo "updated lumina translations."
 # first ensure all translations are known to GIT
 # then create a commit using only the translation files
 # other changes to the source will still be tracked by dpkg-source
-[[ -d "${CURDIR}"/.git ]] && ( git add */i18n/*.ts && git commit */i18n/*.ts \
+[[ -d "${CURDIR}"/.git ]] && ( git add src-qt5/*/*/i18n/*.ts && git commit src-qt5/*/*/i18n/*.ts \
 	-m "merge latest translations from https://github.com/pcbsd/lumina-i18n.git" )
+
 rm -rf lumina-i18n
