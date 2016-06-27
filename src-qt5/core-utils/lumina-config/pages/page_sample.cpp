b0cb61b91a58de6b506c336af6d2328e60c43939
Ken Moore <moorekou@gmail.com>
2016-06-27 15:00:45 -0400

    Add in sample page files for copy/modify later.

---
+++ src-qt5/core-utils/lumina-config/pages/page_sample.cpp
@@ -0,0 +1,42 @@
+//===========================================
+//  Lumina Desktop Source Code
+//  Copyright (c) 2016, Ken Moore
+//  Available under the 3-clause BSD license
+//  See the LICENSE file for full details
+//===========================================
+#include "page_sample.h"
+#include "ui_page_sample.h"
+#include "getPage.h"
+
+//==========
+//    PUBLIC
+//==========
+page_sample::page_sample(QWidget *parent) : PageWidget(parent), ui(new Ui::page_sample()){
+  ui->setupUi(this);
+
+}
+
+page_sample::~page_sample(){
+
+}
+
+//================
+//    PUBLIC SLOTS
+//================
+void page_sample::SaveSettings(){
+
+}
+
+void page_sample::LoadSettings(int){
+  emit HasPendingChanges(false);
+  emit ChangePageTitle( tr("Desktop Settings") );
+
+}
+
+void page_sample::updateIcons(){
+
+}
+
+//=================
+//    PRIVATE SLOTS
+//=================
