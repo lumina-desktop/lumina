c1651e696d153428fe94b19e3550f8c3828a9852
Ken Moore <moorekou@gmail.com>
2016-06-30 09:53:05 -0400

    Fix some whitespace issues with the ls.json.sh script.

--- JsonMenu_Scripts/ls.json.sh
+++ JsonMenu_Scripts/ls.json.sh
@@ -4,16 +4,16 @@ if [ "$1" == "" ] ; then
   DIR=`pwd`
 fi
 
-ls ${DIR} > /tmp/.tmp.lines.$$ 
+ls "${DIR}" > /tmp/.tmp.lines.$$ 
 while read name
 do
   if [ -n "${OUT}" ] ; then
     OUT="${OUT},"
   fi
   if [ -d "${DIR}/${name}" ] ; then
-    OUT="${OUT} \"${name}\" : { \"type\" : \"jsonmenu\", \"exec\" : \"${0} ${DIR}/${name}\", \"icon\":\"folder\"}"
+    OUT="${OUT} \"${name}\" : { \"type\" : \"jsonmenu\", \"exec\" : \"${0} \\\"${DIR}/${name}\\\"\", \"icon\":\"folder\"}"
   else
-    OUT="${OUT} \"${name}\" : { \"type\" : \"item\", \"icon\":\"unknown\", \"action\" : \"xdg-open ${DIR}/${name}\"}"
+    OUT="${OUT} \"${name}\" : { \"type\" : \"item\", \"icon\":\"unknown\", \"action\" : \"xdg-open \\\"${DIR}/${name}\\\"\"}"
   fi
 done < /tmp/.tmp.lines.$$
 rm /tmp/.tmp.lines.$$
