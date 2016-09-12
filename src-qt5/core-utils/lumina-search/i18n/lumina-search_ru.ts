c842b5c66396a33d0ffda6caf5ae72201808a0c5
Nikolay <koljasha@gmail.com>
2016-09-12 22:58:30 +0000

    Translated using Weblate (lumina_SEARCH@ru (generated))
    Currently translated at 96.4% (27 of 28 strings)

--- src-qt5/core-utils/lumina-search/i18n/lumina-search_ru.ts
+++ src-qt5/core-utils/lumina-search/i18n/lumina-search_ru.ts
@@ -1,40 +1,40 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ru">
 <context>
     <name>ConfigUI</name>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="14"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="14"></location>
         <source>Search Configuration</source>
         <translation>Настройка Поиска</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="20"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="20"></location>
         <source>Start Search:</source>
         <translation>Начать Поиск:</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="39"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="39"></location>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="48"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="48"></location>
         <source>Excluded Directories</source>
         <translation>Папки Исключения</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="119"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.ui" line="119"></location>
         <source>Save as Defaults</source>
         <translation>Сохранить по умолчанию</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.cpp" line="41"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.cpp" line="41"></location>
         <source>Select Search Directory</source>
         <translation>Выбрать Папку для Поиска</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.cpp" line="54"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/ConfigUI.cpp" line="54"></location>
         <source>Exclude Directories</source>
         <translation>Исключить Папки</translation>
     </message>
@@ -42,94 +42,94 @@
 <context>
     <name>MainUI</name>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="14"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="14"></location>
         <source>Search For...</source>
         <translation>Поиск...</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="23"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="23"></location>
         <source>Type to search</source>
         <translation>Введите для поиска</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="30"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="30"></location>
         <source>Configure search parameters</source>
         <translation>Настроить параметры поиска</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="40"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="40"></location>
         <source>Stop Active Search</source>
         <translation>Остановить Поиск</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="54"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="54"></location>
         <source>Applications</source>
         <translation>Приложения</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="64"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="64"></location>
         <source>Files or Directories</source>
         <translation>Файлы или Папки</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="88"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="88"></location>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="108"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="108"></location>
         <source>Launch Item</source>
         <translation>Запустить Выбранное</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="119"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="119"></location>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="122"/>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="125"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="122"></location>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="125"></location>
         <source>Save Screenshot</source>
         <translation>Сохранить Снимок Экрана</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="128"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="128"></location>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="133"/>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="136"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="133"></location>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="136"></location>
         <source>Quit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="141"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="141"></location>
         <source>Snap</source>
         <translation>Cнимок</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="144"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.ui" line="144"></location>
         <source>Take new snapshot</source>
         <translation>Сделать новый Снимок</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="29"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="29"></location>
         <source>Esc</source>
         <translation>Esc</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="95"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="95"></location>
         <source>Search: %1 -- Smart: %2</source>
         <translation>Поиск: %1 -- Умный: %2</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="98"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="98"></location>
         <source>Off</source>
         <translation>Выключено</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="98"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/MainUI.cpp" line="98"></location>
         <source>On</source>
         <translation>Включено</translation>
     </message>
@@ -137,17 +137,17 @@
 <context>
     <name>Worker</name>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/Worker.cpp" line="40"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/Worker.cpp" line="40"></location>
         <source>Searching: %1</source>
         <translation>Поиск: %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/Worker.cpp" line="61"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/Worker.cpp" line="61"></location>
         <source>Starting Search: %1</source>
         <translation>Начало поиска: %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/Worker.cpp" line="109"/>
+        <location filename="../../lumina-git/src-qt5/core-utils/lumina-search/Worker.cpp" line="109"></location>
         <source>Search Finished</source>
         <translation>Поиск Окончен</translation>
     </message>
