71a5ac0c327102c3da8667de39de5e90d7b35624
Tommi Nieminen <translator@legisign.org>
2016-09-14 20:33:04 +0000

    Translated using Weblate (lumina_OPEN@fi (generated))
    Currently translated at 100.0% (39 of 39 strings)

--- src-qt5/core/lumina-open/i18n/lumina-open_fi.ts
+++ src-qt5/core/lumina-open/i18n/lumina-open_fi.ts
@@ -1,135 +1,135 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fi">
 <context>
     <name>LFileDialog</name>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="14"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="14"></location>
         <source>Open With...</source>
         <translation>Avaa sovellukseen…</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="68"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="68"></location>
         <source>Preferred</source>
         <translation>Suosittu</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="75"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="75"></location>
         <source>Available</source>
         <translation>Käytettävissä</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="82"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="82"></location>
         <source>Custom</source>
         <translation>Mukautettu</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="131"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="131"></location>
         <source>Binary Location</source>
         <translation>Ohjelmatiedoston sijainti</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="138"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="138"></location>
         <source>Find</source>
         <translation>Etsi</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="199"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="199"></location>
         <source>Set this application as the default </source>
         <translation>Aseta tämä sovellus oletukseksi</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="221"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="221"></location>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="235"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.ui" line="235"></location>
         <source>Cancel</source>
         <translation>Peru</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="40"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="40"></location>
         <source>(Email Link)</source>
         <translation>(Sähköpostilinkki)</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="41"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="41"></location>
         <source>(Internet URL - %1)</source>
         <translation>(Internet-osoite – %1)</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="114"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="114"></location>
         <source>Audio</source>
         <translation>Ääni</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="115"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="115"></location>
         <source>Video</source>
         <translation>Video</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="116"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="116"></location>
         <source>Multimedia</source>
         <translation>Multimedia</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="117"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="117"></location>
         <source>Development</source>
         <translation>Ohjelmakehitys</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="118"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="118"></location>
         <source>Education</source>
         <translation>Kasvatus</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="119"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="119"></location>
         <source>Game</source>
         <translation>Peli</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="120"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="120"></location>
         <source>Graphics</source>
         <translation>Grafiikka</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="121"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="121"></location>
         <source>Network</source>
         <translation>Verkko</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="122"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="122"></location>
         <source>Office</source>
         <translation>Toimisto</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="123"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="123"></location>
         <source>Science</source>
         <translation>Tiede</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="124"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="124"></location>
         <source>Settings</source>
         <translation>Asetukset</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="125"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="125"></location>
         <source>System</source>
         <translation>Järjestelmä</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="126"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="126"></location>
         <source>Utilities</source>
         <translation>Työkalut</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="127"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="127"></location>
         <source>Other</source>
         <translation>Muuta</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="271"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/LFileDialog.cpp" line="271"></location>
         <source>Find Application Binary</source>
         <translation>Etsi ohjelmatiedosto</translation>
     </message>
@@ -137,69 +137,69 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="51"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="51"></location>
         <source>File Error</source>
         <translation>Tiedostovirhe</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="187"/>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="193"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="187"></location>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="193"></location>
         <source>Audio Volume %1%</source>
         <translation>Äänenvoimakkuus %1 %</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="201"/>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="210"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="201"></location>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="210"></location>
         <source>Screen Brightness %1%</source>
         <translation>Näytön kirkkaus %1 %</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="242"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="242"></location>
         <source>Invalid file or URL: %1</source>
         <translation>Virheellinen tiedosto tai verkko-osoite: %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="264"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="264"></location>
         <source>File could not be opened: %1</source>
         <translation>Tiedostoa ei voitu avata: %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="273"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="273"></location>
         <source>Application shortcut is missing the launching information (malformed shortcut): %1</source>
         <translation>Sovelluksen pikakuvakkeesta puuttuvat käynnistystiedot (virheellinen pikakuvake): %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="284"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="284"></location>
         <source>URL shortcut is missing the URL: %1</source>
         <translation>Verkko-osoitteen pikakuvakkeesta puuttuu verkko-osoite: %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="295"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="295"></location>
         <source>Directory shortcut is missing the path to the directory: %1</source>
         <translation>Kansion pikakuvakkeesta puuttuu kansion sijainti: %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="300"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="300"></location>
         <source>Unknown type of shortcut : %1</source>
         <translation>Tuntematon pikakuvaketyyppi: %1</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="362"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="362"></location>
         <source>Binary Missing</source>
-        <translation type="unfinished"></translation>
+        <translation>Ohjelmatiedosto puuttuu</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="362"/>
-        <source>Could not find &quot;%1&quot;. Please ensure it is installed first.</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="362"></location>
+        <source>Could not find "%1". Please ensure it is installed first.</source>
+        <translation>Kohdetta ”%1” ei löytynyt. Varmista ensin, että se on asennettu.</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="412"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="412"></location>
         <source>Application Error</source>
         <translation>Sovellusvirhe</translation>
     </message>
     <message>
-        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="412"/>
+        <location filename="../../lumina-git/src-qt5/core/lumina-open/main.cpp" line="412"></location>
         <source>The following application experienced an error and needed to close:</source>
         <translation>Seuraavat sovellukset kohtasivat virheen ja ne täytyy sulkea:</translation>
     </message>
