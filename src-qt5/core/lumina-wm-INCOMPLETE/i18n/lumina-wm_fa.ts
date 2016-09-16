01d324a8768f72c285356fb842670df7b87e72f7
Hamid Sarmadi <truth@posteo.de>
2016-09-16 00:15:14 +0000

    Translated using Weblate (lumina_WM@fa (generated))
    Currently translated at 87.5% (7 of 8 strings)

--- src-qt5/core/lumina-wm-INCOMPLETE/i18n/lumina-wm_fa.ts
+++ src-qt5/core/lumina-wm-INCOMPLETE/i18n/lumina-wm_fa.ts
@@ -1,45 +1,45 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fa_IR">
 <context>
     <name>LLockScreen</name>
     <message>
-        <location filename="../LLockScreen.ui" line="14"/>
+        <location filename="../LLockScreen.ui" line="14"></location>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>از</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.ui" line="111"/>
+        <location filename="../LLockScreen.ui" line="111"></location>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>گذرواژه</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.ui" line="123"/>
+        <location filename="../LLockScreen.ui" line="123"></location>
         <source>Unlock Session</source>
-        <translation type="unfinished"></translation>
+        <translation>نشست را باز کن</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="43"/>
+        <location filename="../LLockScreen.cpp" line="43"></location>
         <source>Locked by: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>بسته شده بدست: %1</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="125"/>
+        <location filename="../LLockScreen.cpp" line="125"></location>
         <source>Too Many Failures: Try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>شکست‌های بسیار: زمانی دیگر دوباره آزمایش کنید.</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="128"/>
+        <location filename="../LLockScreen.cpp" line="128"></location>
         <source>Password Expired: Contact System Admin</source>
-        <translation type="unfinished"></translation>
+        <translation>گذرواژه بسر آمده: به سرپرستِ سامانه بگویید</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="132"/>
+        <location filename="../LLockScreen.cpp" line="132"></location>
         <source>Failure: %1 Attempts Remaining</source>
-        <translation type="unfinished"></translation>
+        <translation>شکست: %1 تلاش مانده</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="133"/>
+        <location filename="../LLockScreen.cpp" line="133"></location>
         <source>Too Many Failures: Try again in 5 minutes</source>
         <translation type="unfinished"></translation>
     </message>
