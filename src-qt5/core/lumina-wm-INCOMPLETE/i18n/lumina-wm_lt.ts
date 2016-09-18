a8ea0ddeab690d9c240f00f332f6cb772cc84bd9
Moo <moose@mail.ru>
2016-09-18 20:05:53 +0000

    Translated using Weblate (lumina_WM@lt (generated))
    Currently translated at 100.0% (8 of 8 strings)

--- src-qt5/core/lumina-wm-INCOMPLETE/i18n/lumina-wm_lt.ts
+++ src-qt5/core/lumina-wm-INCOMPLETE/i18n/lumina-wm_lt.ts
@@ -1,47 +1,47 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="lt_LT">
 <context>
     <name>LLockScreen</name>
     <message>
-        <location filename="../LLockScreen.ui" line="14"/>
+        <location filename="../LLockScreen.ui" line="14"></location>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Forma</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.ui" line="111"/>
+        <location filename="../LLockScreen.ui" line="111"></location>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Slaptažodis</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.ui" line="123"/>
+        <location filename="../LLockScreen.ui" line="123"></location>
         <source>Unlock Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Atrakinti seansą</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="43"/>
+        <location filename="../LLockScreen.cpp" line="43"></location>
         <source>Locked by: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Užrakino: %1</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="125"/>
+        <location filename="../LLockScreen.cpp" line="125"></location>
         <source>Too Many Failures: Try again later.</source>
-        <translation type="unfinished"></translation>
+        <translation>Per daug nesėkmių: Bandykite dar kartą vėliau.</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="128"/>
+        <location filename="../LLockScreen.cpp" line="128"></location>
         <source>Password Expired: Contact System Admin</source>
-        <translation type="unfinished"></translation>
+        <translation>Baigėsi slaptažodžio galiojimas: Susisiekite su sistemos administratoriumi</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="132"/>
+        <location filename="../LLockScreen.cpp" line="132"></location>
         <source>Failure: %1 Attempts Remaining</source>
-        <translation type="unfinished"></translation>
+        <translation>Nesėkmė: Liko %1 bandymai</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="133"/>
+        <location filename="../LLockScreen.cpp" line="133"></location>
         <source>Too Many Failures: Try again in 5 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>Per daug nesėkmių: Bandykite dar kartą po 5 minučių</translation>
     </message>
 </context>
 </TS>
