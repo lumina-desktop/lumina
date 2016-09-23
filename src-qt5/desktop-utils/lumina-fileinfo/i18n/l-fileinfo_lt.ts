da2b2f290404c8ba113935659d729cb9f867493d
Moo <moose@mail.ru>
2016-09-23 08:50:22 +0000

    Translated using Weblate (l_FILEINFO@lt (generated))
    Currently translated at 100.0% (43 of 43 strings)

--- src-qt5/desktop-utils/lumina-fileinfo/i18n/l-fileinfo_lt.ts
+++ src-qt5/desktop-utils/lumina-fileinfo/i18n/l-fileinfo_lt.ts
@@ -1,225 +1,225 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="lt">
 <context>
     <name>MainUI</name>
     <message>
-        <location filename="../MainUI.ui" line="14"/>
-        <location filename="../MainUI.ui" line="24"/>
-        <location filename="../MainUI.cpp" line="89"/>
+        <location filename="../MainUI.ui" line="14"></location>
+        <location filename="../MainUI.ui" line="24"></location>
+        <location filename="../MainUI.cpp" line="89"></location>
         <source>File Information</source>
         <translation>Failo informacija</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="56"/>
+        <location filename="../MainUI.ui" line="56"></location>
         <source>Owner:</source>
         <translation>Savininkas:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="73"/>
+        <location filename="../MainUI.ui" line="73"></location>
         <source>Group:</source>
         <translation>Grupė:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="90"/>
+        <location filename="../MainUI.ui" line="90"></location>
         <source>Permissions:</source>
         <translation>Leidimai:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="104"/>
+        <location filename="../MainUI.ui" line="104"></location>
         <source>Created:</source>
         <translation>Sukurta:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="111"/>
+        <location filename="../MainUI.ui" line="111"></location>
         <source>Note: The time a file was created might be more recent than the time modified if the file permissions were changed recently.</source>
         <translation>Pastaba: Laikas, kuomet failas buvo sukurtas gali būti vėlesnis nei failo keitimo laikas tuo atveju, jei failo leidimai neseniai buvo pakeisti.</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="124"/>
+        <location filename="../MainUI.ui" line="124"></location>
         <source>Last Modified:</source>
         <translation>Paskutinį kartą keistas:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="131"/>
+        <location filename="../MainUI.ui" line="131"></location>
         <source>Type:</source>
         <translation>Tipas:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="138"/>
+        <location filename="../MainUI.ui" line="138"></location>
         <source>MimeType:</source>
         <translation>MIME tipas:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="185"/>
+        <location filename="../MainUI.ui" line="185"></location>
         <source>File Size:</source>
         <translation>Failo dydis:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="216"/>
-        <location filename="../MainUI.cpp" line="130"/>
+        <location filename="../MainUI.ui" line="216"></location>
+        <location filename="../MainUI.cpp" line="130"></location>
         <source>Edit Shortcut</source>
         <translation>Redaguoti šaukinį</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="239"/>
+        <location filename="../MainUI.ui" line="239"></location>
         <source>Working Dir:</source>
         <translation>Darbinis katalogas:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="246"/>
+        <location filename="../MainUI.ui" line="246"></location>
         <source>Use startup notification</source>
         <translation>Naudoti paleidimo pranešimą</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="260"/>
+        <location filename="../MainUI.ui" line="260"></location>
         <source>Icon:</source>
         <translation>Piktograma:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="267"/>
+        <location filename="../MainUI.ui" line="267"></location>
         <source>Command:</source>
         <translation>Komanda:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="274"/>
+        <location filename="../MainUI.ui" line="274"></location>
         <source>Comment:</source>
         <translation>Komentaras:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="284"/>
+        <location filename="../MainUI.ui" line="284"></location>
         <source>Run in terminal</source>
         <translation>Vykdyti terminale</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="291"/>
+        <location filename="../MainUI.ui" line="291"></location>
         <source>Name:</source>
         <translation>Pavadinimas:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="298"/>
+        <location filename="../MainUI.ui" line="298"></location>
         <source>Options</source>
         <translation>Parinktys</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="336"/>
+        <location filename="../MainUI.ui" line="336"></location>
         <source>No Icon</source>
         <translation>Nėra piktogramos</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="392"/>
+        <location filename="../MainUI.ui" line="392"></location>
         <source>Save</source>
         <translation>Įrašyti</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="405"/>
+        <location filename="../MainUI.ui" line="405"></location>
         <source>Close</source>
         <translation>Užverti</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="59"/>
+        <location filename="../MainUI.cpp" line="59"></location>
         <source>---Calculating---</source>
         <translation>---Skaičiuojama---</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="68"/>
+        <location filename="../MainUI.cpp" line="68"></location>
         <source>Read/Write</source>
         <translation>Skaityti/Rašyti</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="69"/>
+        <location filename="../MainUI.cpp" line="69"></location>
         <source>Read Only</source>
         <translation>TIk skaityti</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="70"/>
+        <location filename="../MainUI.cpp" line="70"></location>
         <source>Write Only</source>
         <translation>Tik rašyti</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="71"/>
+        <location filename="../MainUI.cpp" line="71"></location>
         <source>No Access</source>
         <translation>Nėra prieigos</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="75"/>
+        <location filename="../MainUI.cpp" line="75"></location>
         <source>XDG Shortcut</source>
         <translation>XDG šaukinys</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="76"/>
+        <location filename="../MainUI.cpp" line="76"></location>
         <source>Directory</source>
         <translation>Katalogas</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="77"/>
+        <location filename="../MainUI.cpp" line="77"></location>
         <source>Binary</source>
         <translation>Dvejetainė</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="79"/>
+        <location filename="../MainUI.cpp" line="79"></location>
         <source>Hidden %1</source>
         <translation>Paslėptas %1</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="116"/>
+        <location filename="../MainUI.cpp" line="116"></location>
         <source>URL:</source>
         <translation>URL:</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="237"/>
+        <location filename="../MainUI.cpp" line="237"></location>
         <source>Save Application File</source>
-        <translation type="unfinished"></translation>
+        <translation>Įrašyti programos failą</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="237"/>
+        <location filename="../MainUI.cpp" line="237"></location>
         <source>Application Registrations (*.desktop)</source>
-        <translation type="unfinished"></translation>
+        <translation>Programų įrašai (*.desktop)</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="278"/>
+        <location filename="../MainUI.cpp" line="278"></location>
         <source>Select a binary</source>
         <translation>Pasirinkti dvejetainę</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="281"/>
+        <location filename="../MainUI.cpp" line="281"></location>
         <source>Error</source>
         <translation>Klaida</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="281"/>
+        <location filename="../MainUI.cpp" line="281"></location>
         <source>Invalid selection: Not a valid executable</source>
         <translation>Neteisingas pasirinkimas: Neteisingas vykdomasis</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="293"/>
+        <location filename="../MainUI.cpp" line="293"></location>
         <source>Select a directory</source>
         <translation>Pasirinkti katalogą</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="306"/>
+        <location filename="../MainUI.cpp" line="306"></location>
         <source>Select an icon</source>
         <translation>Pasirinkti piktogramą</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="306"/>
+        <location filename="../MainUI.cpp" line="306"></location>
         <source>Images (%1);; All Files (*)</source>
         <translation>Paveikslai (%1);; Visi failai (*)</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="329"/>
-        <location filename="../MainUI.cpp" line="331"/>
+        <location filename="../MainUI.cpp" line="329"></location>
+        <location filename="../MainUI.cpp" line="331"></location>
         <source> Folders: </source>
         <translation> Aplankai: </translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="329"/>
-        <location filename="../MainUI.cpp" line="331"/>
+        <location filename="../MainUI.cpp" line="329"></location>
+        <location filename="../MainUI.cpp" line="331"></location>
         <source>Files: </source>
         <translation>Failai: </translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="331"/>
+        <location filename="../MainUI.cpp" line="331"></location>
         <source>  Calculating...</source>
         <translation>  Skaičiuojama...</translation>
     </message>
