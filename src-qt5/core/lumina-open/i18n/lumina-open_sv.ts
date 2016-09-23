8014e8dc50192fb8549fb30ccddea1a53af9f568
Luna Jernberg <droidbittin@gmail.com>
2016-09-23 06:15:34 +0000

    Translated using Weblate (lumina_OPEN@sv (generated))
    Currently translated at 100.0% (39 of 39 strings)

--- src-qt5/core/lumina-open/i18n/lumina-open_sv.ts
+++ src-qt5/core/lumina-open/i18n/lumina-open_sv.ts
@@ -1,135 +1,135 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv">
 <context>
     <name>LFileDialog</name>
     <message>
-        <location filename="../LFileDialog.ui" line="14"/>
+        <location filename="../LFileDialog.ui" line="14"></location>
         <source>Open With...</source>
         <translation>Öppna med...</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="68"/>
+        <location filename="../LFileDialog.ui" line="68"></location>
         <source>Preferred</source>
         <translation>Föredras</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="75"/>
+        <location filename="../LFileDialog.ui" line="75"></location>
         <source>Available</source>
         <translation>Tillgänglig</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="82"/>
+        <location filename="../LFileDialog.ui" line="82"></location>
         <source>Custom</source>
         <translation>Anpassad</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="131"/>
+        <location filename="../LFileDialog.ui" line="131"></location>
         <source>Binary Location</source>
         <translation>Binärens Plats</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="138"/>
+        <location filename="../LFileDialog.ui" line="138"></location>
         <source>Find</source>
         <translation>Hitta</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="199"/>
+        <location filename="../LFileDialog.ui" line="199"></location>
         <source>Set this application as the default </source>
         <translation>Sätt detta program som standard</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="221"/>
+        <location filename="../LFileDialog.ui" line="221"></location>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="235"/>
+        <location filename="../LFileDialog.ui" line="235"></location>
         <source>Cancel</source>
         <translation>Avbryt</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="40"/>
+        <location filename="../LFileDialog.cpp" line="40"></location>
         <source>(Email Link)</source>
         <translation>(Email Länk)</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="41"/>
+        <location filename="../LFileDialog.cpp" line="41"></location>
         <source>(Internet URL - %1)</source>
         <translation>(Internet URL - %1)</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="114"/>
+        <location filename="../LFileDialog.cpp" line="114"></location>
         <source>Audio</source>
         <translation>Ljud</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="115"/>
+        <location filename="../LFileDialog.cpp" line="115"></location>
         <source>Video</source>
         <translation>Video</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="116"/>
+        <location filename="../LFileDialog.cpp" line="116"></location>
         <source>Multimedia</source>
         <translation>Multimedia</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="117"/>
+        <location filename="../LFileDialog.cpp" line="117"></location>
         <source>Development</source>
         <translation>Utveckling</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="118"/>
+        <location filename="../LFileDialog.cpp" line="118"></location>
         <source>Education</source>
         <translation>Utbildning</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="119"/>
+        <location filename="../LFileDialog.cpp" line="119"></location>
         <source>Game</source>
         <translation>Spel</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="120"/>
+        <location filename="../LFileDialog.cpp" line="120"></location>
         <source>Graphics</source>
         <translation>Grafik</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="121"/>
+        <location filename="../LFileDialog.cpp" line="121"></location>
         <source>Network</source>
         <translation>Nätverk</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="122"/>
+        <location filename="../LFileDialog.cpp" line="122"></location>
         <source>Office</source>
         <translation>Kontorsprogram</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="123"/>
+        <location filename="../LFileDialog.cpp" line="123"></location>
         <source>Science</source>
         <translation>Vetenskap</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="124"/>
+        <location filename="../LFileDialog.cpp" line="124"></location>
         <source>Settings</source>
         <translation>Inställningar</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="125"/>
+        <location filename="../LFileDialog.cpp" line="125"></location>
         <source>System</source>
         <translation>System</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="126"/>
+        <location filename="../LFileDialog.cpp" line="126"></location>
         <source>Utilities</source>
         <translation>Verktyg</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="127"/>
+        <location filename="../LFileDialog.cpp" line="127"></location>
         <source>Other</source>
         <translation>Annat </translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="271"/>
+        <location filename="../LFileDialog.cpp" line="271"></location>
         <source>Find Application Binary</source>
         <translation>Hitta Programmets Binär </translation>
     </message>
@@ -137,69 +137,69 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="51"/>
+        <location filename="../main.cpp" line="51"></location>
         <source>File Error</source>
         <translation>Filfel</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="187"/>
-        <location filename="../main.cpp" line="193"/>
+        <location filename="../main.cpp" line="187"></location>
+        <location filename="../main.cpp" line="193"></location>
         <source>Audio Volume %1%</source>
         <translation>Ljudvolym %1%</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="201"/>
-        <location filename="../main.cpp" line="210"/>
+        <location filename="../main.cpp" line="201"></location>
+        <location filename="../main.cpp" line="210"></location>
         <source>Screen Brightness %1%</source>
         <translation>Skärmljusstyrka %1%</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="242"/>
+        <location filename="../main.cpp" line="242"></location>
         <source>Invalid file or URL: %1</source>
         <translation>Ogiltig fil eller URL %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="264"/>
+        <location filename="../main.cpp" line="264"></location>
         <source>File could not be opened: %1</source>
         <translation>Filen kunde inte öppnas: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="273"/>
+        <location filename="../main.cpp" line="273"></location>
         <source>Application shortcut is missing the launching information (malformed shortcut): %1</source>
         <translation>Program genväg saknar startinformation (missbildad genväg): %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="284"/>
+        <location filename="../main.cpp" line="284"></location>
         <source>URL shortcut is missing the URL: %1</source>
         <translation>URL genväg saknas URL: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="295"/>
+        <location filename="../main.cpp" line="295"></location>
         <source>Directory shortcut is missing the path to the directory: %1</source>
         <translation>Genvägen till katalogen saknar sökvägen till katalogen :%1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="300"/>
+        <location filename="../main.cpp" line="300"></location>
         <source>Unknown type of shortcut : %1</source>
         <translation>Okänd typ av genväg: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="362"/>
+        <location filename="../main.cpp" line="362"></location>
         <source>Binary Missing</source>
-        <translation type="unfinished"></translation>
+        <translation>Binär saknas</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="362"/>
-        <source>Could not find &quot;%1&quot;. Please ensure it is installed first.</source>
-        <translation type="unfinished"></translation>
+        <location filename="../main.cpp" line="362"></location>
+        <source>Could not find "%1". Please ensure it is installed first.</source>
+        <translation>Kunde inte hitta "%1". Se till att du har detta programmet installerat först.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="412"/>
+        <location filename="../main.cpp" line="412"></location>
         <source>Application Error</source>
         <translation>Program fel</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="412"/>
+        <location filename="../main.cpp" line="412"></location>
         <source>The following application experienced an error and needed to close:</source>
         <translation>Följande program upplevde ett fel och behövde stängas:</translation>
     </message>
