8dfd8f5a78b76490f11148b4388727cbce3f6a51
George Ember <sk8harddiefast@gmail.com>
2016-09-23 23:44:21 +0000

    Translated using Weblate (l_SCREENSHOT@el (generated))
    Currently translated at 100.0% (24 of 24 strings)

--- src-qt5/desktop-utils/lumina-screenshot/i18n/l-screenshot_el.ts
+++ src-qt5/desktop-utils/lumina-screenshot/i18n/l-screenshot_el.ts
@@ -1,133 +1,133 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="l-screenshot_el">
 <context>
     <name>ImageEditor</name>
     <message>
-        <location filename="../ImageEditor.cpp" line="14"/>
+        <location filename="../ImageEditor.cpp" line="14"></location>
         <source>Zoom In</source>
-        <translation type="unfinished"></translation>
+        <translation>Μεγένθυση</translation>
     </message>
     <message>
-        <location filename="../ImageEditor.cpp" line="15"/>
+        <location filename="../ImageEditor.cpp" line="15"></location>
         <source>Zoom Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Σμίκρυνση</translation>
     </message>
 </context>
 <context>
     <name>MainUI</name>
     <message>
-        <location filename="../MainUI.ui" line="14"/>
-        <location filename="../MainUI.ui" line="162"/>
-        <location filename="../MainUI.ui" line="295"/>
+        <location filename="../MainUI.ui" line="14"></location>
+        <location filename="../MainUI.ui" line="162"></location>
+        <location filename="../MainUI.ui" line="295"></location>
         <source>Take Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Πάρτε στιγμιότυπο οθόνης</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="37"/>
+        <location filename="../MainUI.ui" line="37"></location>
         <source>New Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Νέο Στιγμιότυπο οθόνης</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="43"/>
+        <location filename="../MainUI.ui" line="43"></location>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ρυθμίσεις</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="124"/>
+        <location filename="../MainUI.ui" line="124"></location>
         <source>Include Borders</source>
-        <translation type="unfinished"></translation>
+        <translation>Συμπερίληψη περιθωρίων</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="149"/>
+        <location filename="../MainUI.ui" line="149"></location>
         <source> Seconds</source>
-        <translation type="unfinished"></translation>
+        <translation> Δευτερόλεπτα</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="152"/>
+        <location filename="../MainUI.ui" line="152"></location>
         <source>Delay </source>
-        <translation type="unfinished"></translation>
+        <translation>Καθυστέρηση </translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="172"/>
+        <location filename="../MainUI.ui" line="172"></location>
         <source>View/Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Προβολή/Επεξεργασία</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="212"/>
-        <location filename="../MainUI.ui" line="306"/>
+        <location filename="../MainUI.ui" line="212"></location>
+        <location filename="../MainUI.ui" line="306"></location>
         <source>Save As</source>
-        <translation type="unfinished"></translation>
+        <translation>Αποθήκευση Ως</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="222"/>
+        <location filename="../MainUI.ui" line="222"></location>
         <source>Launch Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Εκκίνηση Επεξεργαστή</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="245"/>
+        <location filename="../MainUI.ui" line="245"></location>
         <source>Crop</source>
-        <translation type="unfinished"></translation>
+        <translation>Περικοπή</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="255"/>
+        <location filename="../MainUI.ui" line="255"></location>
         <source>Resize</source>
-        <translation type="unfinished"></translation>
+        <translation>Αλλαγή μεγέθους</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="282"/>
+        <location filename="../MainUI.ui" line="282"></location>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Αρχείο</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="298"/>
+        <location filename="../MainUI.ui" line="298"></location>
         <source>Ctrl+N</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+N</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="322"/>
+        <location filename="../MainUI.ui" line="322"></location>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Κλείσιμο</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="325"/>
+        <location filename="../MainUI.ui" line="325"></location>
         <source>Esc</source>
-        <translation type="unfinished"></translation>
+        <translation>Esc</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="67"/>
+        <location filename="../MainUI.ui" line="67"></location>
         <source>Entire Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Ολόκληρη την Συνεδρία</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="81"/>
+        <location filename="../MainUI.ui" line="81"></location>
         <source>Single Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Μόνο αυτό το παράθυρο</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="74"/>
+        <location filename="../MainUI.ui" line="74"></location>
         <source>Single Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Μόνο αυτή την οθόνη</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="77"/>
+        <location filename="../MainUI.cpp" line="77"></location>
         <source>Save Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Αποθήκευση Στιγμιοτύπου οθόνης</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="309"/>
+        <location filename="../MainUI.ui" line="309"></location>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="317"/>
+        <location filename="../MainUI.ui" line="317"></location>
         <source>Quick Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Γρήγορη Αποθήκευση</translation>
     </message>
     <message>
-        <location filename="../MainUI.cpp" line="77"/>
+        <location filename="../MainUI.cpp" line="77"></location>
         <source>PNG Files (*.png);;AllFiles (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Αρχεία PNG (*.png);;ΌλαΤαΑρχεία (*)</translation>
     </message>
 </context>
 </TS>
