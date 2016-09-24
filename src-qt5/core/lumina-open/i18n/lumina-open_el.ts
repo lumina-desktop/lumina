79fc4b76cfc6b58320c0359c63b14bc10f9fd33d
George Ember <sk8harddiefast@gmail.com>
2016-09-24 00:03:31 +0000

    Translated using Weblate (lumina_OPEN@el (generated))
    Currently translated at 100.0% (39 of 39 strings)

--- src-qt5/core/lumina-open/i18n/lumina-open_el.ts
+++ src-qt5/core/lumina-open/i18n/lumina-open_el.ts
@@ -1,207 +1,207 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el">
 <context>
     <name>LFileDialog</name>
     <message>
-        <location filename="../LFileDialog.ui" line="14"/>
+        <location filename="../LFileDialog.ui" line="14"></location>
         <source>Open With...</source>
         <translation>Άνοιγμα με...</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="68"/>
+        <location filename="../LFileDialog.ui" line="68"></location>
         <source>Preferred</source>
         <translation>Προτιμώμενη</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="75"/>
+        <location filename="../LFileDialog.ui" line="75"></location>
         <source>Available</source>
         <translation>Διαθέσιμα</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="82"/>
+        <location filename="../LFileDialog.ui" line="82"></location>
         <source>Custom</source>
         <translation>Προσαρμοσμένο</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="131"/>
+        <location filename="../LFileDialog.ui" line="131"></location>
         <source>Binary Location</source>
-        <translation type="unfinished"></translation>
+        <translation>Τοποθεσία Εφαρμογής</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="138"/>
+        <location filename="../LFileDialog.ui" line="138"></location>
         <source>Find</source>
         <translation>Εύρεση</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="199"/>
+        <location filename="../LFileDialog.ui" line="199"></location>
         <source>Set this application as the default </source>
         <translation>Ορισμός αυτής της εφαρμογής ως προεπιλεγμένης</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="221"/>
+        <location filename="../LFileDialog.ui" line="221"></location>
         <source>OK</source>
         <translation>ΟΚ</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="235"/>
+        <location filename="../LFileDialog.ui" line="235"></location>
         <source>Cancel</source>
-        <translation>Άκυρο</translation>
+        <translation>Ακύρωση</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="40"/>
+        <location filename="../LFileDialog.cpp" line="40"></location>
         <source>(Email Link)</source>
-        <translation type="unfinished"></translation>
+        <translation>(Σύνδεσμος Ηλεκτρονικού Ταχυδρομείου)</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="41"/>
+        <location filename="../LFileDialog.cpp" line="41"></location>
         <source>(Internet URL - %1)</source>
-        <translation type="unfinished"></translation>
+        <translation>(URL Διαδικτύου -  %1)</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="114"/>
+        <location filename="../LFileDialog.cpp" line="114"></location>
         <source>Audio</source>
         <translation>Ήχος</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="115"/>
+        <location filename="../LFileDialog.cpp" line="115"></location>
         <source>Video</source>
         <translation>Βίντεο</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="116"/>
+        <location filename="../LFileDialog.cpp" line="116"></location>
         <source>Multimedia</source>
         <translation>Πολυμέσα</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="117"/>
+        <location filename="../LFileDialog.cpp" line="117"></location>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Ανάπτυξη</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="118"/>
+        <location filename="../LFileDialog.cpp" line="118"></location>
         <source>Education</source>
-        <translation type="unfinished"></translation>
+        <translation>Εκπαίδευση</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="119"/>
+        <location filename="../LFileDialog.cpp" line="119"></location>
         <source>Game</source>
         <translation>Παιχνίδια</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="120"/>
+        <location filename="../LFileDialog.cpp" line="120"></location>
         <source>Graphics</source>
         <translation>Γραφικά</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="121"/>
+        <location filename="../LFileDialog.cpp" line="121"></location>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Δίκτυο</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="122"/>
+        <location filename="../LFileDialog.cpp" line="122"></location>
         <source>Office</source>
-        <translation type="unfinished"></translation>
+        <translation>Σουΐτα Γραφείου</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="123"/>
+        <location filename="../LFileDialog.cpp" line="123"></location>
         <source>Science</source>
-        <translation type="unfinished"></translation>
+        <translation>Επιστήμη</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="124"/>
+        <location filename="../LFileDialog.cpp" line="124"></location>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ρυθμίσεις</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="125"/>
+        <location filename="../LFileDialog.cpp" line="125"></location>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Σύστημα</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="126"/>
+        <location filename="../LFileDialog.cpp" line="126"></location>
         <source>Utilities</source>
-        <translation type="unfinished"></translation>
+        <translation>Εργαλεία</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="127"/>
+        <location filename="../LFileDialog.cpp" line="127"></location>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>Άλλο</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="271"/>
+        <location filename="../LFileDialog.cpp" line="271"></location>
         <source>Find Application Binary</source>
-        <translation type="unfinished"></translation>
+        <translation>Βρείτε Εκτελέσιμη Εφαρμογή</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="51"/>
+        <location filename="../main.cpp" line="51"></location>
         <source>File Error</source>
-        <translation>Σφάλμα αρχείου</translation>
+        <translation>Σφάλμα Αρχείου</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="187"/>
-        <location filename="../main.cpp" line="193"/>
+        <location filename="../main.cpp" line="187"></location>
+        <location filename="../main.cpp" line="193"></location>
         <source>Audio Volume %1%</source>
         <translation>Ένταση ήχου %1%</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="201"/>
-        <location filename="../main.cpp" line="210"/>
+        <location filename="../main.cpp" line="201"></location>
+        <location filename="../main.cpp" line="210"></location>
         <source>Screen Brightness %1%</source>
         <translation>Φωτεινότητα οθόνης %1%</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="242"/>
+        <location filename="../main.cpp" line="242"></location>
         <source>Invalid file or URL: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Μη έγκυρο αρχείο ή URL: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="264"/>
+        <location filename="../main.cpp" line="264"></location>
         <source>File could not be opened: %1</source>
-        <translation>Αδυναμία ανοίγματος αρχείου%1%</translation>
+        <translation>Αδυναμία ανοίγματος αρχείου: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="273"/>
+        <location filename="../main.cpp" line="273"></location>
         <source>Application shortcut is missing the launching information (malformed shortcut): %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Οι πληροφορίες εκτέλεσης λείπουν απο την συντόμευση της εφαρμογής (Δυσλειτουργία συντόμευσης): %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="284"/>
+        <location filename="../main.cpp" line="284"></location>
         <source>URL shortcut is missing the URL: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Απο την URL συντόμευση λείπει το URL: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="295"/>
+        <location filename="../main.cpp" line="295"></location>
         <source>Directory shortcut is missing the path to the directory: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Απο την συντόμευση του καταλόγου λείπει η διαδρομή προς τον κατάλογο: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="300"/>
+        <location filename="../main.cpp" line="300"></location>
         <source>Unknown type of shortcut : %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Άγνωστος τύπος συντόμευσης: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="362"/>
+        <location filename="../main.cpp" line="362"></location>
         <source>Binary Missing</source>
-        <translation type="unfinished"></translation>
+        <translation>Το Εκτελέσιμο λείπει</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="362"/>
-        <source>Could not find &quot;%1&quot;. Please ensure it is installed first.</source>
-        <translation type="unfinished"></translation>
+        <location filename="../main.cpp" line="362"></location>
+        <source>Could not find "%1". Please ensure it is installed first.</source>
+        <translation>Αδυναμία έυρεσης "%1".  Παρακαλώ βεβαιωθείτε ότι έχει ήδη εγκατασταθεί.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="412"/>
+        <location filename="../main.cpp" line="412"></location>
         <source>Application Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Πρόβλημα Εφαρμογής</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="412"/>
+        <location filename="../main.cpp" line="412"></location>
         <source>The following application experienced an error and needed to close:</source>
-        <translation type="unfinished"></translation>
+        <translation>Η παρακάτω εφαρμογή αντιμετώπισε ένα σφάλμα και πρέπει να τερματιστεί:</translation>
     </message>
 </context>
 </TS>
