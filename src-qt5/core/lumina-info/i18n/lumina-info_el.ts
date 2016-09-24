a0038914706b05d34e956c3e208d3af9abbbe03d
George Ember <sk8harddiefast@gmail.com>
2016-09-24 00:12:59 +0000

    Translated using Weblate (lumina_INFO@el (generated))
    Currently translated at 100.0% (18 of 18 strings)

--- src-qt5/core/lumina-info/i18n/lumina-info_el.ts
+++ src-qt5/core/lumina-info/i18n/lumina-info_el.ts
@@ -1,95 +1,95 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el">
 <context>
     <name>MainUI</name>
     <message>
-        <location filename="../MainUI.ui" line="14"/>
+        <location filename="../MainUI.ui" line="14"></location>
         <source>Lumina Desktop Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Πληροφορίες Γραφικού Περιβάλλοντος Lumina</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="60"/>
+        <location filename="../MainUI.ui" line="60"></location>
         <source>General</source>
         <translation>Γενικά</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="89"/>
+        <location filename="../MainUI.ui" line="89"></location>
         <source>Source Repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Πηγή Αποθετηρίου</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="124"/>
+        <location filename="../MainUI.ui" line="124"></location>
         <source>Lumina Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Η Ιστοσελίδα του Lumina</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="152"/>
+        <location filename="../MainUI.ui" line="152"></location>
         <source>Bug Reports</source>
-        <translation type="unfinished"></translation>
+        <translation>Aναφορές Σφαλμάτων (Bugs)</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="179"/>
+        <location filename="../MainUI.ui" line="179"></location>
         <source>Desktop Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Έκδοση Γραφικού Περιβάλλοντος</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="202"/>
+        <location filename="../MainUI.ui" line="202"></location>
         <source>OS Build:</source>
-        <translation type="unfinished"></translation>
+        <translation>Έκδοση Δόμησης OS:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="218"/>
+        <location filename="../MainUI.ui" line="218"></location>
         <source>Qt Version:</source>
         <translation>Έκδοση Qt:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="276"/>
+        <location filename="../MainUI.ui" line="276"></location>
         <source>Ask the Community</source>
-        <translation type="unfinished"></translation>
+        <translation>Ρωτήστε την Κοινότητα</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="233"/>
+        <location filename="../MainUI.ui" line="233"></location>
         <source>View Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Προβολή Πληροφοριών</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="298"/>
+        <location filename="../MainUI.ui" line="298"></location>
         <source>License</source>
-        <translation>Άδεια χρήσης</translation>
+        <translation>Άδεια Χρήσης</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="315"/>
+        <location filename="../MainUI.ui" line="315"></location>
         <source>Acknowledgements</source>
-        <translation type="unfinished"></translation>
+        <translation>Ευχαριστίες</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="333"/>
+        <location filename="../MainUI.ui" line="333"></location>
         <source>Project Lead:</source>
-        <translation type="unfinished"></translation>
+        <translation>Επικεφαλής του Έργου:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="340"/>
-        <source>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <location filename="../MainUI.ui" line="340"></location>
+        <source>&lt;a href="https://github.com/beanpole135"&gt;Ken Moore&lt;/a&gt;</source>
+        <translation>&lt;a href="https://github.com/beanpole135"&gt;&lt;a href="https://github.com/beanpole135"&gt;Ken Moore&lt;/a&gt;&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="353"/>
+        <location filename="../MainUI.ui" line="353"></location>
         <source>Contributors:</source>
-        <translation type="unfinished"></translation>
+        <translation>Συντελεστές:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="366"/>
+        <location filename="../MainUI.ui" line="366"></location>
         <source>Sponsors:</source>
         <translation>Χορηγοί</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="428"/>
-        <source>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Open in web browser&lt;a&gt;</source>
-        <translation type="unfinished"></translation>
+        <location filename="../MainUI.ui" line="428"></location>
+        <source>&lt;a href="https://github.com/pcbsd/lumina/graphs/contributors"&gt;Open in web browser&lt;a&gt;</source>
+        <translation>&lt;a href="https://github.com/pcbsd/lumina/graphs/contributors"&gt;Άνοιγμα στον Φυλλομετρητή (Browser)&lt;a&gt;</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="454"/>
+        <location filename="../MainUI.ui" line="454"></location>
         <source>Close</source>
         <translation>Κλείσιμο</translation>
     </message>
