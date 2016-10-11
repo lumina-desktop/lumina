bceb1ec76fafddb6583378257a4120061dfab4bd
Ken Moore <ken@ixsystems.com>
2016-10-11 09:05:04 -0400

    Update the links for the Gitter chat and bug tracker in lumina-info. Fixes #277

--- src-qt5/core/lumina-info/main.cpp
+++ src-qt5/core/lumina-info/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char ** argv)
     LSingleApplication a(argc, argv, "lumina-info"); //loads translations inside constructor
       if( !a.isPrimaryProcess()){ return 0; }
     //qDebug() << "Set Application Name";
-    a.setApplicationName("About Lumina-DE");
+    a.setApplicationName("About Lumina");
     //qDebug() << "Load Theme Engine";
     LuminaThemeEngine themes(&a);
     //qDebug() << "Start the UI";
