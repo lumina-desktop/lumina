85353ba9941d7971d19698dca54bdc946b4ec9e9
Ken Moore <ken@ixsystems.com>
2016-10-21 10:48:55 -0400

    Tag version 1.1.0 in the source tree

--- src-qt5/core/libLumina/LuminaUtils.cpp
+++ src-qt5/core/libLumina/LuminaUtils.cpp
@@ -54,7 +54,7 @@ inline QStringList ProcessRun(QString cmd, QStringList args){
 //  LUtils Functions
 //=============
 QString LUtils::LuminaDesktopVersion(){ 
-  QString ver = "1.0.1";
+  QString ver = "1.1.0";
   #ifdef GIT_VERSION
   ver.append( QString(" (Git Revision: %1)").arg(GIT_VERSION) );
   #endif
