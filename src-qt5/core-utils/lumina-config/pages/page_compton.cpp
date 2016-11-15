0afad0dbbae4440ec311e9682f62686889afbcce
Ken Moore <ken@ixsystems.com>
2016-11-15 10:10:16 -0500

    Add the new option for detection GPU accel to the compositor page.

--- src-qt5/core-utils/lumina-config/pages/page_compton.cpp
+++ src-qt5/core-utils/lumina-config/pages/page_compton.cpp
@@ -15,6 +15,7 @@ page_compton::page_compton(QWidget *parent) : PageWidget(parent), ui(new Ui::pag
   ui->setupUi(this);
   connect(ui->text_file, SIGNAL(textChanged()), this, SLOT(settingChanged()) );
   connect(ui->check_disablecompton, SIGNAL(toggled(bool)), this, SLOT(settingChanged()) );
+  connect(ui->check_GPUverify, SIGNAL(toggled(bool)), this, SLOT(settingChanged()) );
   updateIcons();
 }
 
@@ -29,6 +30,7 @@ void page_compton::SaveSettings(){
   emit HasPendingChanges(false);
   QSettings settings("lumina-desktop","sessionsettings");
     settings.setValue("enableCompositing", !ui->check_disablecompton->isChecked());
+    settings.setValue("compositingWithGpuAccelOnly", ui->check_GPUverify->isChecked());
   QString set = QString(getenv("XDG_CONFIG_HOME"))+"/lumina-desktop/compton.conf";
   LUtils::writeFile(set, ui->text_file->toPlainText().split("\n"),true);
 }
@@ -38,6 +40,7 @@ void page_compton::LoadSettings(int){
   emit ChangePageTitle( tr("Compositor Settings") );
   QSettings settings("lumina-desktop","sessionsettings");
     ui->check_disablecompton->setChecked( !settings.value("enableCompositing", true).toBool() );
+    ui->check_GPUverify->setChecked( settings.value("compositingWithGpuAccelOnly", true).toBool() );
   QString set = QString(getenv("XDG_CONFIG_HOME"))+"/lumina-desktop/compton.conf";
   qDebug() << "Load Compton settings:" << set;
   ui->text_file->setPlainText( LUtils::readFile(set).join("\n") );
