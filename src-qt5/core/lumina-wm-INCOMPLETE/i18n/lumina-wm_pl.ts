2e1700ca7ee2a9fb74932d2b83ac0c9908ee4e19
Patryk <patryk.sidzina+weblate@gmail.com>
2016-11-23 13:17:13 +0000

    Translated using Weblate (lumina_WM@pl (generated))
    Currently translated at 71.4% (5 of 7 strings)

--- src-qt5/core/lumina-wm-INCOMPLETE/i18n/lumina-wm_pl.ts
+++ src-qt5/core/lumina-wm-INCOMPLETE/i18n/lumina-wm_pl.ts
@@ -1,42 +1,42 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pl_PL">
 <context>
     <name>LLockScreen</name>
     <message>
-        <location filename="../LLockScreen.ui" line="14"/>
+        <location filename="../LLockScreen.ui" line="14"></location>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Formularz</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.ui" line="114"/>
+        <location filename="../LLockScreen.ui" line="114"></location>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Hasło</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.ui" line="126"/>
+        <location filename="../LLockScreen.ui" line="126"></location>
         <source>Unlock Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Odblokuj Sesję</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="39"/>
+        <location filename="../LLockScreen.cpp" line="39"></location>
         <source>Locked by: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Zablokowane przez: %1</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="76"/>
+        <location filename="../LLockScreen.cpp" line="76"></location>
         <source>Too Many Failures</source>
-        <translation type="unfinished"></translation>
+        <translation>Za Dużo Błędów</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="76"/>
+        <location filename="../LLockScreen.cpp" line="76"></location>
         <source>Wait %1 Minutes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Zaczekaj %1 Minut</translation>
     </message>
     <message>
-        <location filename="../LLockScreen.cpp" line="77"/>
+        <location filename="../LLockScreen.cpp" line="77"></location>
         <source>Failed Attempts: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieudanych Prób: %1</translation>
     </message>
 </context>
 </TS>
