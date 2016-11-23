abab2f87bb7e2cb013bfc77bba6345f8f81014da
Patryk <patryk.sidzina+weblate@gmail.com>
2016-11-23 14:16:35 +0000

    Translated using Weblate (lumina_INFO@pl (generated))
    Currently translated at 94.4% (17 of 18 strings)

--- src-qt5/core/lumina-info/i18n/lumina-info_pl.ts
+++ src-qt5/core/lumina-info/i18n/lumina-info_pl.ts
@@ -1,95 +1,95 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pl">
 <context>
     <name>MainUI</name>
     <message>
-        <location filename="../MainUI.ui" line="14"/>
+        <location filename="../MainUI.ui" line="14"></location>
         <source>Lumina Desktop Information</source>
         <translation>Informacje pulpitu Lumina</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="60"/>
+        <location filename="../MainUI.ui" line="60"></location>
         <source>General</source>
         <translation>Ogólne</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="89"/>
+        <location filename="../MainUI.ui" line="89"></location>
         <source>Source Repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Repozytorium Żródłowe</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="124"/>
+        <location filename="../MainUI.ui" line="124"></location>
         <source>Lumina Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Strona WWW Lumina</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="152"/>
+        <location filename="../MainUI.ui" line="152"></location>
         <source>Bug Reports</source>
-        <translation type="unfinished"></translation>
+        <translation>Zgłaszanie Błędów</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="179"/>
+        <location filename="../MainUI.ui" line="179"></location>
         <source>Desktop Version:</source>
         <translation>Wersja pulpitu:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="202"/>
+        <location filename="../MainUI.ui" line="202"></location>
         <source>OS Build:</source>
         <translation>Wersja systemu operacyjnego:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="218"/>
+        <location filename="../MainUI.ui" line="218"></location>
         <source>Qt Version:</source>
         <translation>Wersja Qt:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="276"/>
+        <location filename="../MainUI.ui" line="276"></location>
         <source>Ask the Community</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Zadaj Pytanie</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="233"/>
+        <location filename="../MainUI.ui" line="233"></location>
         <source>View Information</source>
         <translation>Wyświetl informacje</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="298"/>
+        <location filename="../MainUI.ui" line="298"></location>
         <source>License</source>
         <translation>Licencja</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="315"/>
+        <location filename="../MainUI.ui" line="315"></location>
         <source>Acknowledgements</source>
         <translation>Podziękowania</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="333"/>
+        <location filename="../MainUI.ui" line="333"></location>
         <source>Project Lead:</source>
         <translation>Lider projektu:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="340"/>
-        <source>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</source>
-        <translation>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</translation>
+        <location filename="../MainUI.ui" line="340"></location>
+        <source>&lt;a href="https://github.com/beanpole135"&gt;Ken Moore&lt;/a&gt;</source>
+        <translation>&lt;a href="https://github.com/beanpole135"&gt;Ken Moore&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="353"/>
+        <location filename="../MainUI.ui" line="353"></location>
         <source>Contributors:</source>
         <translation>Współtwórcy:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="366"/>
+        <location filename="../MainUI.ui" line="366"></location>
         <source>Sponsors:</source>
         <translation>Sponsorzy:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="428"/>
-        <source>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Open in web browser&lt;a&gt;</source>
-        <translation>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Otwórz w przeglądarce internetowej&lt;a&gt;</translation>
+        <location filename="../MainUI.ui" line="428"></location>
+        <source>&lt;a href="https://github.com/pcbsd/lumina/graphs/contributors"&gt;Open in web browser&lt;a&gt;</source>
+        <translation>&lt;a href="https://github.com/pcbsd/lumina/graphs/contributors"&gt;Otwórz w przeglądarce internetowej&lt;a&gt;</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="454"/>
+        <location filename="../MainUI.ui" line="454"></location>
         <source>Close</source>
         <translation>Zamknij</translation>
     </message>
