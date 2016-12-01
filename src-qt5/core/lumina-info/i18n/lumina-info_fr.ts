68d0d317c407a74019b92732a70b0306821a9719
Anil Mir <archnux-weblate@yahoo.fr>
2016-12-01 16:10:52 +0000

    Translated using Weblate (lumina_INFO@fr (generated))
    Currently translated at 100.0% (18 of 18 strings)

--- src-qt5/core/lumina-info/i18n/lumina-info_fr.ts
+++ src-qt5/core/lumina-info/i18n/lumina-info_fr.ts
@@ -1,95 +1,95 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr">
 <context>
     <name>MainUI</name>
     <message>
-        <location filename="../MainUI.ui" line="14"/>
+        <location filename="../MainUI.ui" line="14"></location>
         <source>Lumina Desktop Information</source>
-        <translation>Informations sur l&apos;environnement Lumina</translation>
+        <translation>Informations sur l'environnement Lumina</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="60"/>
+        <location filename="../MainUI.ui" line="60"></location>
         <source>General</source>
         <translation>Général</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="89"/>
+        <location filename="../MainUI.ui" line="89"></location>
         <source>Source Repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Dépôt Source</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="124"/>
+        <location filename="../MainUI.ui" line="124"></location>
         <source>Lumina Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Site Web de Lumina</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="152"/>
+        <location filename="../MainUI.ui" line="152"></location>
         <source>Bug Reports</source>
-        <translation type="unfinished"></translation>
+        <translation>Rapports de Bugs</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="179"/>
+        <location filename="../MainUI.ui" line="179"></location>
         <source>Desktop Version:</source>
-        <translation>Version de l&apos;environnement:</translation>
+        <translation>Version de l'environnement:</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="202"/>
+        <location filename="../MainUI.ui" line="202"></location>
         <source>OS Build:</source>
-        <translation>Build du Système d&apos;Exploitation :</translation>
+        <translation>Build du Système d'Exploitation :</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="218"/>
+        <location filename="../MainUI.ui" line="218"></location>
         <source>Qt Version:</source>
         <translation>Version de Qt :</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="276"/>
+        <location filename="../MainUI.ui" line="276"></location>
         <source>Ask the Community</source>
-        <translation type="unfinished"></translation>
+        <translation>Demander à la Communauté</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="233"/>
+        <location filename="../MainUI.ui" line="233"></location>
         <source>View Information</source>
         <translation>Afficher les informations</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="298"/>
+        <location filename="../MainUI.ui" line="298"></location>
         <source>License</source>
         <translation>Licence</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="315"/>
+        <location filename="../MainUI.ui" line="315"></location>
         <source>Acknowledgements</source>
         <translation>Remerciements</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="333"/>
+        <location filename="../MainUI.ui" line="333"></location>
         <source>Project Lead:</source>
         <translation>Chef de projet :</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="340"/>
-        <source>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</source>
-        <translation>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</translation>
+        <location filename="../MainUI.ui" line="340"></location>
+        <source>&lt;a href="https://github.com/beanpole135"&gt;Ken Moore&lt;/a&gt;</source>
+        <translation>&lt;a href="https://github.com/beanpole135"&gt;Ken Moore&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="353"/>
+        <location filename="../MainUI.ui" line="353"></location>
         <source>Contributors:</source>
         <translation>Contributeurs :</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="366"/>
+        <location filename="../MainUI.ui" line="366"></location>
         <source>Sponsors:</source>
         <translation>Sponsors :</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="428"/>
-        <source>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Open in web browser&lt;a&gt;</source>
-        <translation>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Ouvrir dans un navigateur web&lt;a&gt;</translation>
+        <location filename="../MainUI.ui" line="428"></location>
+        <source>&lt;a href="https://github.com/pcbsd/lumina/graphs/contributors"&gt;Open in web browser&lt;a&gt;</source>
+        <translation>&lt;a href="https://github.com/pcbsd/lumina/graphs/contributors"&gt;Ouvrir dans un navigateur web&lt;a&gt;</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="454"/>
+        <location filename="../MainUI.ui" line="454"></location>
         <source>Close</source>
         <translation>Fermer</translation>
     </message>
