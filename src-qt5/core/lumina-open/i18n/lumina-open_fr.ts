e0fd512481f0eaa6561877f2be3eecb41d5584fc
Anil Mir <archnux-weblate@yahoo.fr>
2016-12-01 10:47:53 +0000

    Translated using Weblate (lumina_OPEN@fr (generated))
    Currently translated at 100.0% (39 of 39 strings)

--- src-qt5/core/lumina-open/i18n/lumina-open_fr.ts
+++ src-qt5/core/lumina-open/i18n/lumina-open_fr.ts
@@ -1,135 +1,135 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="fr">
 <context>
     <name>LFileDialog</name>
     <message>
-        <location filename="../LFileDialog.ui" line="14"/>
+        <location filename="../LFileDialog.ui" line="14"></location>
         <source>Open With...</source>
         <translation>Ouvir avec...</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="68"/>
+        <location filename="../LFileDialog.ui" line="68"></location>
         <source>Preferred</source>
         <translation>Favori</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="75"/>
+        <location filename="../LFileDialog.ui" line="75"></location>
         <source>Available</source>
         <translation>Disponible</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="82"/>
+        <location filename="../LFileDialog.ui" line="82"></location>
         <source>Custom</source>
         <translation>Personnalisé</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="131"/>
+        <location filename="../LFileDialog.ui" line="131"></location>
         <source>Binary Location</source>
         <translation>Emplacement du binaire :</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="138"/>
+        <location filename="../LFileDialog.ui" line="138"></location>
         <source>Find</source>
         <translation>Trouver</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="199"/>
+        <location filename="../LFileDialog.ui" line="199"></location>
         <source>Set this application as the default </source>
         <translation>Mettre cette application par défaut</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="221"/>
+        <location filename="../LFileDialog.ui" line="221"></location>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.ui" line="235"/>
+        <location filename="../LFileDialog.ui" line="235"></location>
         <source>Cancel</source>
         <translation>Annuler</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="40"/>
+        <location filename="../LFileDialog.cpp" line="40"></location>
         <source>(Email Link)</source>
         <translation>(Lien e-mail)</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="41"/>
+        <location filename="../LFileDialog.cpp" line="41"></location>
         <source>(Internet URL - %1)</source>
         <translation>(URL Internet - %1)</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="114"/>
+        <location filename="../LFileDialog.cpp" line="114"></location>
         <source>Audio</source>
         <translation>Son</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="115"/>
+        <location filename="../LFileDialog.cpp" line="115"></location>
         <source>Video</source>
         <translation>Vidéo</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="116"/>
+        <location filename="../LFileDialog.cpp" line="116"></location>
         <source>Multimedia</source>
         <translation>Multimédia</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="117"/>
+        <location filename="../LFileDialog.cpp" line="117"></location>
         <source>Development</source>
         <translation>Développement</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="118"/>
+        <location filename="../LFileDialog.cpp" line="118"></location>
         <source>Education</source>
         <translation>Éducation</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="119"/>
+        <location filename="../LFileDialog.cpp" line="119"></location>
         <source>Game</source>
         <translation>Jeu</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="120"/>
+        <location filename="../LFileDialog.cpp" line="120"></location>
         <source>Graphics</source>
         <translation>Graphiques</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="121"/>
+        <location filename="../LFileDialog.cpp" line="121"></location>
         <source>Network</source>
         <translation>Réseau</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="122"/>
+        <location filename="../LFileDialog.cpp" line="122"></location>
         <source>Office</source>
         <translation>Bureau</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="123"/>
+        <location filename="../LFileDialog.cpp" line="123"></location>
         <source>Science</source>
         <translation>Science</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="124"/>
+        <location filename="../LFileDialog.cpp" line="124"></location>
         <source>Settings</source>
         <translation>Paramètres</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="125"/>
+        <location filename="../LFileDialog.cpp" line="125"></location>
         <source>System</source>
         <translation>Système</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="126"/>
+        <location filename="../LFileDialog.cpp" line="126"></location>
         <source>Utilities</source>
         <translation>Utilitaires</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="127"/>
+        <location filename="../LFileDialog.cpp" line="127"></location>
         <source>Other</source>
         <translation>Autre</translation>
     </message>
     <message>
-        <location filename="../LFileDialog.cpp" line="271"/>
+        <location filename="../LFileDialog.cpp" line="271"></location>
         <source>Find Application Binary</source>
         <translation>Rechcher les Applications binaires</translation>
     </message>
@@ -137,71 +137,71 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="51"/>
+        <location filename="../main.cpp" line="51"></location>
         <source>File Error</source>
         <translation>Erreur de fichier</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="187"/>
-        <location filename="../main.cpp" line="193"/>
+        <location filename="../main.cpp" line="187"></location>
+        <location filename="../main.cpp" line="193"></location>
         <source>Audio Volume %1%</source>
         <translation>Volume audio %1%</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="201"/>
-        <location filename="../main.cpp" line="210"/>
+        <location filename="../main.cpp" line="201"></location>
+        <location filename="../main.cpp" line="210"></location>
         <source>Screen Brightness %1%</source>
-        <translation>Luminosité de l&apos;écran %1%</translation>
+        <translation>Luminosité de l'écran %1%</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="242"/>
+        <location filename="../main.cpp" line="242"></location>
         <source>Invalid file or URL: %1</source>
         <translation>Fichier ou URL Invalide: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="264"/>
+        <location filename="../main.cpp" line="264"></location>
         <source>File could not be opened: %1</source>
-        <translation>Impossible d&apos;ouvrir le fichier: %1</translation>
+        <translation>Impossible d'ouvrir le fichier: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="273"/>
+        <location filename="../main.cpp" line="273"></location>
         <source>Application shortcut is missing the launching information (malformed shortcut): %1</source>
         <translation>Information de lancement manquant (raccourci malformé): %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="284"/>
+        <location filename="../main.cpp" line="284"></location>
         <source>URL shortcut is missing the URL: %1</source>
-        <translation>Le raccourci URL est introuvable dans l&apos;URL : %1</translation>
+        <translation>Le raccourci URL est introuvable dans l'URL : %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="295"/>
+        <location filename="../main.cpp" line="295"></location>
         <source>Directory shortcut is missing the path to the directory: %1</source>
         <translation>Chemin vers le répertoire manquant dans le raccourci du répertoire: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="300"/>
+        <location filename="../main.cpp" line="300"></location>
         <source>Unknown type of shortcut : %1</source>
         <translation>Type du raccourci inconnu: %1</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="362"/>
+        <location filename="../main.cpp" line="362"></location>
         <source>Binary Missing</source>
-        <translation type="unfinished"></translation>
+        <translation>Binaire Manquant</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="362"/>
-        <source>Could not find &quot;%1&quot;. Please ensure it is installed first.</source>
-        <translation type="unfinished"></translation>
+        <location filename="../main.cpp" line="362"></location>
+        <source>Could not find "%1". Please ensure it is installed first.</source>
+        <translation>"%1" non trouvé. Vérifiez qu'il soit bien installé.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="412"/>
+        <location filename="../main.cpp" line="412"></location>
         <source>Application Error</source>
-        <translation>Erreur de l&apos;application</translation>
+        <translation>Erreur de l'application</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="412"/>
+        <location filename="../main.cpp" line="412"></location>
         <source>The following application experienced an error and needed to close:</source>
-        <translation>L&apos;application suivante a rencontré une erreur et doit être fermée:</translation>
+        <translation>L'application suivante a rencontré une erreur et doit être fermée:</translation>
     </message>
 </context>
 </TS>
