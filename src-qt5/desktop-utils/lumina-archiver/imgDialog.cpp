9f98c7dcba441d9b9c9215b57bd67c28bc637652
Ken Moore <ken@ixsystems.com>
2017-01-03 09:34:41 -0500

    Block off the SIGINFO/SIGUSR1 section with __Linux__ definition checks. This should fix compiling on Linux systems which do not have the SIGINFO signal defined.

--- src-qt5/desktop-utils/lumina-archiver/imgDialog.cpp
+++ src-qt5/desktop-utils/lumina-archiver/imgDialog.cpp
@@ -133,8 +133,12 @@ void imgDialog::getProcStatus(){
   if(pidlist.isEmpty()){ return; }
   int pid = pidlist.first().simplified().toInt(); //just use the first pid - the pgrep should be detailed enough to only match one
   //qDebug() << "Sending signal to show status on PID:" << pid;
-  if(BSD_signal){ ::kill(pid, SIGINFO); } //On BSD systems, the INFO signal is used to poke dd for status updates
-  else{ ::kill(pid, SIGUSR1); } //On linux systems, the USR1 signal is used to poke dd for status updates
+#ifndef __Linux__
+  ::kill(pid, SIGINFO); //On BSD systems, the INFO signal is used to poke dd for status updates
+#else
+  //LINUX systems do not even have a SIGINFO defined - need to block this off with defines...
+  ::kill(pid, SIGUSR1);  //On linux systems, the USR1 signal is used to poke dd for status updates
+#endif
   //Now update the elapsed time on the UI
     int elapsed = startTime.secsTo( QDateTime::currentDateTime() );
     int min = elapsed/60;
