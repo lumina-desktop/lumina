1822d6f3c7b1c4f75868d2d74f0dbe0d988dd372
Arnar Mar Sig <antab@antab.is>
2017-02-06 17:39:38 +0000

    Fix memory leak.

--- src-qt5/core-utils/lumina-config/pages/page_defaultapps.cpp
+++ src-qt5/core-utils/lumina-config/pages/page_defaultapps.cpp
@@ -66,7 +66,7 @@ void page_defaultapps::LoadSettings(int){
   //qDebug() << "Mime List:\n" << defMimeList.join("\n");
   defMimeList.sort(); //sort by group/mime
   //Now fill the tree by group/mime
-  QTreeWidgetItem *group = new QTreeWidgetItem(0); //nothing at the moment
+  QTreeWidgetItem *group = nullptr;
   QString ccat;
   for(int i=0; i<defMimeList.length(); i++){
     //Get the info from this entry
@@ -76,7 +76,7 @@ void page_defaultapps::LoadSettings(int){
     QString def = defMimeList[i].section("::::",2,2);
     QString comment = defMimeList[i].section("::::",3,50);
     //Now check if this is a new category
-    if(ccat!=cat){
+    if(group == nullptr || ccat!=cat){
 	    //New group
 	    group = new QTreeWidgetItem(0);
 	    group->setText(0, cat); //add translations for known/common groups later
