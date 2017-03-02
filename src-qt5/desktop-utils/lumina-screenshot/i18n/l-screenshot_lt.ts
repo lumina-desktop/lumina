150854c98916f5dced6374a79e32b63ab0bd6466
Moo <moose@mail.ru>
2017-03-02 13:26:20 +0000

    Translated using Weblate (l_SCREENSHOT@lt (generated))
    Currently translated at 100.0% (38 of 38 strings)

--- src-qt5/desktop-utils/lumina-screenshot/i18n/l-screenshot_lt.ts
+++ src-qt5/desktop-utils/lumina-screenshot/i18n/l-screenshot_lt.ts
@@ -39,77 +39,77 @@
     <message>
         <location filename="../MainUI.ui" line="20"/>
         <source>Lumina Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Lumina ekrano kopija</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="87"/>
         <source>Open screenshot with an application</source>
-        <translation type="unfinished"></translation>
+        <translation>Atverti ekrano kopiją, naudojant programą</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="90"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Atverti</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="103"/>
         <source>Copy screenshot to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopijuoti ekrano kopiją į iškarpinę</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="106"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopijuoti</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="109"/>
         <source>Ctrl+C</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl(Vald)+C</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="135"/>
         <source>Resize screenshot to selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakeisti ekrano kopijos dydį iki žymėjimo</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="157"/>
         <source>Crop screenshot to selection</source>
-        <translation type="unfinished"></translation>
+        <translation>Apkirpti ekrano kopiją iki žymėjimo</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="160"/>
         <source>&amp;Crop</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Apkirpti</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="293"/>
         <source>Screen Capture Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ekrano fotografavimo nustatymai</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="341"/>
         <source> Sec Delay</source>
-        <translation type="unfinished"></translation>
+        <translation> sek. delsa</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="434"/>
         <source>General Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Bendri nustatymai</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="455"/>
         <source>Show popup warnings about unsaved screenshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodyti iškylančiuosius įspėjimus apie neįrašytas ekrano kopijas</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="488"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>Įrankių juosta</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="515"/>
         <source>Capture</source>
-        <translation type="unfinished"></translation>
+        <translation>Fotografuoti</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="521"/>
@@ -124,7 +124,7 @@
     <message>
         <location filename="../MainUI.ui" line="548"/>
         <source>Close Application</source>
-        <translation type="unfinished"></translation>
+        <translation>Užverti programą</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="551"/>
@@ -149,12 +149,12 @@
     <message>
         <location filename="../MainUI.cpp" line="38"/>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodinys</translation>
     </message>
     <message>
         <location filename="../MainUI.cpp" line="39"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nustatymai</translation>
     </message>
     <message>
         <location filename="../MainUI.cpp" line="94"/>
@@ -174,12 +174,12 @@
     <message>
         <location filename="../MainUI.cpp" line="264"/>
         <source>Unsaved Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Neįrašyta ekrano kopija</translation>
     </message>
     <message>
         <location filename="../MainUI.cpp" line="265"/>
         <source>The current screenshot has not been saved yet. Do you want to save or discard your changes?</source>
-        <translation type="unfinished"></translation>
+        <translation>Esama ekrano kopija kol kas nebuvo neįrašyta. Norite įrašyti ar atmesti savo pakeitimus?</translation>
     </message>
     <message>
         <location filename="../MainUI.ui" line="532"/>
