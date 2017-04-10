77a4237283b69bb8706a5d3720f8751cc43445dd
q5sys <jt@obs-sec.com>
2017-04-09 22:30:29 -0400

    add commented out method for icon size in vertical taskmanager

--- src-qt5/core/lumina-desktop/panel-plugins/taskmanager/LTaskManagerPlugin.h
+++ src-qt5/core/lumina-desktop/panel-plugins/taskmanager/LTaskManagerPlugin.h
@@ -29,6 +29,8 @@ class LTaskManagerPlugin : public LPPlugin{
 public:
 	LTaskManagerPlugin(QWidget *parent=0, QString id="taskmanager", bool horizontal=true);
 	~LTaskManagerPlugin();
+//	int vertsizeicon;
+//	int *dpi;  //this comes from the PCDM dpi
 
 private:
 	QList<LTaskButton*> BUTTONS; //to keep track of the current buttons
@@ -61,8 +63,14 @@ public slots:
 	    this->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
 	    this->layout()->setAlignment(Qt::AlignTop);
 	    QSize sz(this->width(), this->width());
+//            QSize sz(this->width(), this->height());  //we want to increase the width but not the height of the icons
 	    for(int i=0; i<BUTTONS.length(); i++){
 	      BUTTONS[i]->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+//	      BUTTONS[i]->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);	      
+//		if( dpi = 196 ){ int vertsizeicon = ; BUTTONS[i]->setIconSize(vertsizeicon);}
+//		elseif( dpi = 144 ) { int vertsizeicon = ; BUTTONS[i]->setIconSize(vertsizeicon);}
+//		elseif( dpi = 96 ) { int vertsizeicon = ; BUTTONS[i]->setIconSize(vertsizeicon);}
+//		elseif( dpi = 48 ) { int vertsizeicon = ; BUTTONS[i]->setIconSize)vertsizeicon);}
 	      BUTTONS[i]->setIconSize(sz);
 	    }
 	  }
