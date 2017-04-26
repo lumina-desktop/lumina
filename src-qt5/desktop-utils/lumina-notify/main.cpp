b2fbedf9c45d34aa3ce2080cf60e3a9fbe6ffb6a
q5sys <jt@obs-sec.com>
2017-04-25 21:17:59 -0400

    update qnotify for variable output

--- src-qt5/desktop-utils/lumina-notify/main.cpp
+++ src-qt5/desktop-utils/lumina-notify/main.cpp
@@ -1,19 +1,24 @@
 //-------------------------------------------------
-// Created by q5sys
+// Created by q5sys (JT)
 // Released under MIT License 2017-03-08
 // A Simple GUI Dialog Program
 //-------------------------------------------------
 
 #include <QApplication>
 #include <QMessageBox>
+#include <QDebug>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    int answer;
     QMessageBox *messageBox = new QMessageBox;
         messageBox->setText(argv[1]);
-        QPushButton *pushButtonOk = messageBox->addButton(argv[2], QMessageBox::YesRole);
-        messageBox->QDialog::setWindowTitle(argv[3]);
-    messageBox->show();
-    return a.exec();
+        QPushButton *pushButtonOk = messageBox->addButton(argv[2], QMessageBox::AcceptRole);
+        QPushButton *pushButtonNo = messageBox->addButton(argv[3], QMessageBox::RejectRole);
+        messageBox->QDialog::setWindowTitle(argv[4]);
+        messageBox->show();
+        if(messageBox->exec() == QMessageBox::AcceptRole){ answer = 0; QTextStream cout(stdout); cout << answer;}
+        else { answer = 1; QTextStream cout(stdout); cout << answer;}
 }
