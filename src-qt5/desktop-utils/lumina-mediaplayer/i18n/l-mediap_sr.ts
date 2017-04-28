ff4b91a4da20fb54b747da055e3622110dc99cbe
Ken Moore <ken@ixsystems.com>
2017-04-28 00:24:20 -0400

    Oops - move lumina-notify back to the desktop-utils category (JT already turned it on by default) Also add all the translation files for lumina-mediaplayer.

---
+++ src-qt5/desktop-utils/lumina-mediaplayer/i18n/l-mediap_sr.ts
@@ -0,0 +1,370 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sr_RS">
+<context>
+    <name>MainUI</name>
+    <message>
+        <location filename="../mainUI.ui" line="14"/>
+        <location filename="../mainUI.cpp" line="261"/>
+        <source>Media Player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="50"/>
+        <location filename="../mainUI.ui" line="261"/>
+        <location filename="../mainUI.cpp" line="601"/>
+        <source>Now Playing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="74"/>
+        <source>(No Running Video)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="105"/>
+        <source>Playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="167"/>
+        <source>up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="183"/>
+        <source>down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="267"/>
+        <source>Current Song</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="303"/>
+        <source>TITLE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="327"/>
+        <source>ARTIST</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="337"/>
+        <source>ALBUM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="368"/>
+        <source>Love this song</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="384"/>
+        <source>Tired of this song (will not play for a month)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="397"/>
+        <source>Ban this song (will never play again)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="417"/>
+        <source>View details about song (launches web browser)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="448"/>
+        <source>Current Station</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="479"/>
+        <source>Delete current station</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="482"/>
+        <source>rm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="492"/>
+        <source>Create new station</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="495"/>
+        <source>add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="512"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="521"/>
+        <source>Pandora Account Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="527"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="537"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="557"/>
+        <source>&lt;a href=https://www.pandora.com/account/register&gt;Need an account?&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="573"/>
+        <source>Audio Quality</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="583"/>
+        <source>Proxy URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="593"/>
+        <source>Control Proxy URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="618"/>
+        <source>Apply Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="656"/>
+        <source>Audio Driver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="681"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="688"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="699"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="726"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="731"/>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="736"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="741"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="746"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="751"/>
+        <source>VolUp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="754"/>
+        <source>Raise audio volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="759"/>
+        <source>VolDown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="762"/>
+        <source>Lower audio volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="767"/>
+        <source>Close Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="770"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="784"/>
+        <source>Close to tray when active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="789"/>
+        <source>From current artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="792"/>
+        <source>Create station from current artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="797"/>
+        <source>From current song</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="800"/>
+        <source>Create station from current song</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="811"/>
+        <source>Show song notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="816"/>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="819"/>
+        <source>Search for a new station</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="831"/>
+        <location filename="../mainUI.cpp" line="252"/>
+        <source>Pandora Radio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="834"/>
+        <source>Stream from Pandora Radio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="842"/>
+        <source>Local Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.ui" line="845"/>
+        <source>Play Local Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="91"/>
+        <source>Please install the `pianobar` utility to enable this functionality</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="94"/>
+        <source>Stream music from the Pandora online radio service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="107"/>
+        <source>Low</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="108"/>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="109"/>
+        <source>High</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="329"/>
+        <source>Open Multimedia Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="376"/>
+        <source>Now Playing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="441"/>
+        <source>[PLAYBACK ERROR]
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="458"/>
+        <source>Media Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="460"/>
+        <source>Media Stalled...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="462"/>
+        <source>Media Buffering...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="546"/>
+        <source>Pandora: Create Station</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="546"/>
+        <source>Search Term</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="624"/>
+        <source>Pandora Question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainUI.cpp" line="629"/>
+        <source>Pandora Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PianoBarProcess</name>
+    <message>
+        <location filename="../PianoBarProcess.cpp" line="358"/>
+        <source>Could not find any matches. Please try a different search term</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
