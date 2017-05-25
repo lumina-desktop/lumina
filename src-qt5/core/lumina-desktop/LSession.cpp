1140e4075b3c661940134de7ccafeb4cd2daac88
Ken Moore <ken@ixsystems.com>
2017-05-25 16:05:10 -0400

    Disable the VirtualBox fullscreen detection/fix logic After testing this more robustly and with more variations, the issue appears to be so ingrained into the Fluxbox state/property event handling that anything we do from the Lumina side is quickly overwritten/ignored by Fluxbox.

--- src-qt5/core/lumina-desktop/LSession.cpp
+++ src-qt5/core/lumina-desktop/LSession.cpp
@@ -686,7 +686,7 @@ void LSession::WindowPropertyEvent(WId win){
   //Emit the single-app signal if the window in question is one used by the task manager
   if(RunningApps.contains(win)){
     if(DEBUG){ qDebug() << "Single-window property event"; }
-    if( XCB->WindowClass(win).contains("VirtualBox")){
+    /*if( XCB->WindowClass(win).contains("VirtualBox")){
       qDebug() << "Found VirtualBox Window:";
       QList<LXCB::WINDOWSTATE> states = XCB->WM_Get_Window_States(win);
       if(states.contains(LXCB::S_FULLSCREEN) && !states.contains(LXCB::S_HIDDEN)){
@@ -694,7 +694,7 @@ void LSession::WindowPropertyEvent(WId win){
         XCB->WM_Set_Window_Type(win, QList<LXCB::WINDOWTYPE>() << LXCB::T_NORMAL << LXCB::T_UTILITY );
         XCB->RestoreWindow(win);
       }
-    }
+    }*/
     //emit WindowListEvent();
     WindowPropertyEvent(); //Run through the entire routine for window checks
   }else if(RunningTrayApps.contains(win)){
