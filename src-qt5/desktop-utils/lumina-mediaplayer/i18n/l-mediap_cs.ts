7be0aec0bfbda3c7a3a5376482260c2593a07426
Pavel Borecki <pavel.borecki@gmail.com>
2017-07-21 16:23:54 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (71 of 71 strings)
    
    Translation: lumina/lumina-mediaplayer
    Translate-URL: http://weblate.trueos.org/projects/lumina/lumina-mediaplayer/cs/

--- src-qt5/desktop-utils/lumina-mediaplayer/i18n/l-mediap_cs.ts
+++ src-qt5/desktop-utils/lumina-mediaplayer/i18n/l-mediap_cs.ts
@@ -7,356 +7,357 @@
         <location filename="../mainUI.ui" line="14"/>
         <location filename="../mainUI.cpp" line="285"/>
         <source>Media Player</source>
-        <translation type="unfinished"></translation>
+        <translation>Přehrávač multimédií</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="50"/>
         <location filename="../mainUI.ui" line="261"/>
         <location filename="../mainUI.cpp" line="631"/>
         <source>Now Playing</source>
-        <translation type="unfinished"></translation>
+        <translation>Nyní se přehrává</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="74"/>
         <source>(No Running Video)</source>
-        <translation type="unfinished"></translation>
+        <translation>(Žádné přehrávané video)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="105"/>
         <source>Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Seznam k přehrávání</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="167"/>
         <source>up</source>
-        <translation type="unfinished"></translation>
+        <translation>nahoru</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="183"/>
         <source>down</source>
-        <translation type="unfinished"></translation>
+        <translation>dolů</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="267"/>
         <source>Current Song</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuální skladba</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="303"/>
         <source>TITLE</source>
-        <translation type="unfinished"></translation>
+        <translation>NÁZEV</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="327"/>
         <source>ARTIST</source>
-        <translation type="unfinished"></translation>
+        <translation>UMĚLEC</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="337"/>
         <source>ALBUM</source>
-        <translation type="unfinished"></translation>
+        <translation>ALBUM</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="368"/>
         <source>Love this song</source>
-        <translation type="unfinished"></translation>
+        <translation>Skladba se mi líbí</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="384"/>
         <source>Tired of this song (will not play for a month)</source>
-        <translation type="unfinished"></translation>
+        <translation>Skladba už mne nebaví (po dobu jednoho měsíce ji neuslyšíte)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="397"/>
         <source>Ban this song (will never play again)</source>
-        <translation type="unfinished"></translation>
+        <translation>Skladbu už nechci slyšet (už nikdy nebude přehrávána)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="417"/>
         <source>View details about song (launches web browser)</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobrazit podrobnosti o skladbě (spustí webový prohlížeč)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="448"/>
         <source>Current Station</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuální stanice</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="479"/>
         <source>Delete current station</source>
-        <translation type="unfinished"></translation>
+        <translation>Smazat aktuální stanici</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="482"/>
         <source>rm</source>
-        <translation type="unfinished"></translation>
+        <translation>smazat</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="492"/>
         <source>Create new station</source>
-        <translation type="unfinished"></translation>
+        <translation>Vytvořit novou stanici</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="495"/>
         <source>add</source>
-        <translation type="unfinished"></translation>
+        <translation>přidat</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="512"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavení</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="521"/>
         <source>Pandora Account Login</source>
-        <translation type="unfinished"></translation>
+        <translation>Přihlášení k účtu na službě Pandora</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="527"/>
         <source>Email</source>
-        <translation type="unfinished"></translation>
+        <translation>E-mail</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="537"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Heslo</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="557"/>
         <source>&lt;a href=https://www.pandora.com/account/register&gt;Need an account?&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;a href=https://www.pandora.com/account/register&gt;Nemáte ještě účet?&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="573"/>
         <source>Audio Quality</source>
-        <translation type="unfinished"></translation>
+        <translation>Kvalita zvuku</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="583"/>
         <source>Proxy URL</source>
-        <translation type="unfinished"></translation>
+        <translation>URL adresa proxy</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="593"/>
         <source>Control Proxy URL</source>
-        <translation type="unfinished"></translation>
+        <translation>URL řídící proxy</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="618"/>
         <source>Apply Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Použít nastavení</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="656"/>
         <source>Audio Driver</source>
-        <translation type="unfinished"></translation>
+        <translation>Ovladač zvuku</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="681"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Soubor</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="688"/>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobrazit</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="699"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>Nástrojová lišta</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="726"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Přehrát</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="731"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>Pozastavit</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="736"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Zastavit</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="741"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Následující</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="746"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Předchozí</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="751"/>
         <source>VolUp</source>
-        <translation type="unfinished"></translation>
+        <translation>Zesílit</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="754"/>
         <source>Raise audio volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Přidat na hlasitosti zvuku</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="759"/>
         <source>VolDown</source>
-        <translation type="unfinished"></translation>
+        <translation>Ztlumit</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="762"/>
         <source>Lower audio volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Ubrat na hlasitosti zvuku</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="767"/>
         <source>Close Application</source>
-        <translation type="unfinished"></translation>
+        <translation>Zavřít aplikaci</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="770"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="784"/>
         <source>Close to tray when active</source>
-        <translation type="unfinished"></translation>
+        <translation>Když je aktivní, zminimalizovat do oznamovací oblasti systémového panelu</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="789"/>
         <source>From current artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Od stávajícího umělce</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="792"/>
         <source>Create station from current artist</source>
-        <translation type="unfinished"></translation>
+        <translation>Vytvořit stanici od stávajícího umělce</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="797"/>
         <source>From current song</source>
-        <translation type="unfinished"></translation>
+        <translation>Od stávající skladby</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="800"/>
         <source>Create station from current song</source>
-        <translation type="unfinished"></translation>
+        <translation>Vytvořit stanici od stávající skladby</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="811"/>
         <source>Show song notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobrazovat oznámení skladby</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="816"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Hledat…</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="819"/>
         <source>Search for a new station</source>
-        <translation type="unfinished"></translation>
+        <translation>Hledat novou stanici</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="831"/>
         <location filename="../mainUI.cpp" line="276"/>
         <source>Pandora Radio</source>
-        <translation type="unfinished"></translation>
+        <translation>Rádio Pandora</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="834"/>
         <source>Stream from Pandora Radio</source>
-        <translation type="unfinished"></translation>
+        <translation>Proudové vysílání z rádia Pandora</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="842"/>
         <source>Local Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Soubory na počítači</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="845"/>
         <source>Play Local Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Přehrávat soubory na počítači</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="105"/>
         <source>Please install the `pianobar` utility to enable this functionality</source>
-        <translation type="unfinished"></translation>
+        <translation>Aby bylo možné zapnout tuto funkci, je třeba nainstalovat nástroj „pianobar“</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="109"/>
         <source>Stream music from the Pandora online radio service</source>
-        <translation type="unfinished"></translation>
+        <translation>Proudové vysílání hudby ze služby internetového rádia Pandora</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="122"/>
         <source>Low</source>
-        <translation type="unfinished"></translation>
+        <translation>Nízké</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="123"/>
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Střední</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="124"/>
         <source>High</source>
-        <translation type="unfinished"></translation>
+        <translation>Vysoké</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="358"/>
         <source>Open Multimedia Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Otevřít soubory s multimédii</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="406"/>
         <source>Now Playing:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nyní se přehrává:</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="471"/>
         <source>[PLAYBACK ERROR]
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>[CHYBA PŘEHRÁVÁNÍ]
+%1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="488"/>
         <source>Media Loading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Načítání média…</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="490"/>
         <source>Media Stalled...</source>
-        <translation type="unfinished"></translation>
+        <translation>Médium se zastavilo…</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="492"/>
         <source>Media Buffering...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukládání média do vyrovnávací paměti…</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="576"/>
         <source>Pandora: Create Station</source>
-        <translation type="unfinished"></translation>
+        <translation>Pandora: vytvořit stanici</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="576"/>
         <source>Search Term</source>
-        <translation type="unfinished"></translation>
+        <translation>Hledat výraz</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="654"/>
         <source>Pandora Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Dotaz z Pandora</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="659"/>
         <source>Pandora Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba v Pandora</translation>
     </message>
 </context>
 <context>
@@ -364,7 +365,7 @@
     <message>
         <location filename="../PianoBarProcess.cpp" line="358"/>
         <source>Could not find any matches. Please try a different search term</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepodařilo se najít nic odpovídajícího. Zkuste hledat jiný výraz</translation>
     </message>
 </context>
 </TS>
