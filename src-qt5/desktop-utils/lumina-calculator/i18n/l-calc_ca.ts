ce1d76200e815b4479bafa34723120cd961da01c
Davidmp <rbndavid@gmail.com>
2017-07-21 15:28:17 +0000

    Translated using Weblate (Catalan)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: lumina/lumina-calculator
    Translate-URL: http://weblate.trueos.org/projects/lumina/lumina-calculator/ca/

--- src-qt5/desktop-utils/lumina-calculator/i18n/l-calc_ca.ts
+++ src-qt5/desktop-utils/lumina-calculator/i18n/l-calc_ca.ts
@@ -7,102 +7,102 @@
         <location filename="../mainUI.ui" line="14"/>
         <location filename="../mainUI.cpp" line="53"/>
         <source>Calculator</source>
-        <translation type="unfinished"></translation>
+        <translation>Calculadora</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="657"/>
         <source>Advanced Operations</source>
-        <translation type="unfinished"></translation>
+        <translation>Operacions avançades</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="83"/>
         <source>Percentage %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Percentatge %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="85"/>
         <source>Power %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Potència %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="87"/>
         <source>Base-10 Exponential %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Base-10 exponencial %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="89"/>
         <source>Exponential %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Exponencial %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="91"/>
         <source>Constant Pi %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Constant de Pi %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="94"/>
         <source>Square Root %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Arrel quadrada %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="96"/>
         <source>Logarithm %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Logaritme %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="98"/>
         <source>Natural Log %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Log natural %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="101"/>
         <source>Sine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Sinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="103"/>
         <source>Cosine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cosinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="105"/>
         <source>Tangent %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Tangent %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="108"/>
         <source>Arc Sine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Arc sinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="110"/>
         <source>Arc Cosine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Arc cosinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="112"/>
         <source>Arc Tangent %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Arc tangent %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="115"/>
         <source>Hyperbolic Sine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Sinus hiperbòlic %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="117"/>
         <source>Hyperbolic Cosine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cosinus hiperbòlic %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="119"/>
         <source>Hyperbolic Tangent %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Tangent hiperbòlica %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="182"/>
         <source>Save Calculator History</source>
-        <translation type="unfinished"></translation>
+        <translation>Desa l'historial de la calculadora</translation>
     </message>
 </context>
 </TS>
