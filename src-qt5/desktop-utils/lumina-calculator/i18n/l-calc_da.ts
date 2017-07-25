984d3473fb5fb0c0887430fc33c405a5d95f49e6
scootergrisen <scootergrisen@gmail.com>
2017-07-25 21:38:20 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: lumina/lumina-calculator
    Translate-URL: http://weblate.trueos.org/projects/lumina/lumina-calculator/da/

--- src-qt5/desktop-utils/lumina-calculator/i18n/l-calc_da.ts
+++ src-qt5/desktop-utils/lumina-calculator/i18n/l-calc_da.ts
@@ -12,7 +12,7 @@
     <message>
         <location filename="../mainUI.ui" line="657"/>
         <source>Advanced Operations</source>
-        <translation>Avanceret handlinger</translation>
+        <translation>Avancerede operationer</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="83"/>
@@ -27,7 +27,7 @@
     <message>
         <location filename="../mainUI.cpp" line="87"/>
         <source>Base-10 Exponential %1</source>
-        <translation>Base-10 Eksponentiel %1</translation>
+        <translation>Base-10 eksponentiel %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="89"/>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../mainUI.cpp" line="91"/>
         <source>Constant Pi %1</source>
-        <translation>Konstanten Pi %1</translation>
+        <translation>Konstanten pi %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="94"/>
@@ -52,7 +52,7 @@
     <message>
         <location filename="../mainUI.cpp" line="98"/>
         <source>Natural Log %1</source>
-        <translation>Naturlig Log %1</translation>
+        <translation>Naturlig log %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="101"/>
@@ -72,37 +72,37 @@
     <message>
         <location filename="../mainUI.cpp" line="108"/>
         <source>Arc Sine %1</source>
-        <translation>Arc Sinus %1</translation>
+        <translation>Arc sinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="110"/>
         <source>Arc Cosine %1</source>
-        <translation>Arc Cosinus %1</translation>
+        <translation>Arc cosinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="112"/>
         <source>Arc Tangent %1</source>
-        <translation>Arc Tangent %1</translation>
+        <translation>Arc tangent %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="115"/>
         <source>Hyperbolic Sine %1</source>
-        <translation>Hyperbolisk Sinus %1</translation>
+        <translation>Hyperbolisk sinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="117"/>
         <source>Hyperbolic Cosine %1</source>
-        <translation>Hyperbolisk Cosinus %1</translation>
+        <translation>Hyperbolisk cosinus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="119"/>
         <source>Hyperbolic Tangent %1</source>
-        <translation>Hyperbolisk Tangent %1</translation>
+        <translation>Hyperbolisk tangent %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="182"/>
         <source>Save Calculator History</source>
-        <translation>Gem lommeregnerhistorik</translation>
+        <translation>Gem lommeregnerens historik</translation>
     </message>
 </context>
 </TS>
