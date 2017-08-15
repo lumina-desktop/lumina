5e33803ce906de7833166c6ede567a4e9d1b1fb6
Zackary Welch <zwelch@ixsystems.com>
2017-08-15 17:10:33 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 18.3% (13 of 71 strings)
    
    Translation: lumina/lumina-mediaplayer
    Translate-URL: http://weblate.trueos.org/projects/lumina/lumina-mediaplayer/pt_BR/

--- src-qt5/desktop-utils/lumina-mediaplayer/i18n/l-mediap_pt_BR.ts
+++ src-qt5/desktop-utils/lumina-mediaplayer/i18n/l-mediap_pt_BR.ts
@@ -154,7 +154,7 @@
     <message>
         <location filename="../mainUI.ui" line="681"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Arquivo</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="688"/>
@@ -219,7 +219,7 @@
     <message>
         <location filename="../mainUI.ui" line="770"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="784"/>
