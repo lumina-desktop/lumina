b0fce29a9fedcc1784858a1346f85c0e1f666380
Tommi Nieminen <translator@legisign.org>
2017-09-20 10:04:05 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: lumina/lumina-calculator
    Translate-URL: http://weblate.trueos.org/projects/lumina/lumina-calculator/fi/

--- src-qt5/desktop-utils/lumina-calculator/i18n/l-calc_fi.ts
+++ src-qt5/desktop-utils/lumina-calculator/i18n/l-calc_fi.ts
@@ -7,102 +7,102 @@
         <location filename="../mainUI.ui" line="14"/>
         <location filename="../mainUI.cpp" line="53"/>
         <source>Calculator</source>
-        <translation type="unfinished"></translation>
+        <translation>Laskin</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="657"/>
         <source>Advanced Operations</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisätoiminnot</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="83"/>
         <source>Percentage %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Osuus %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="85"/>
         <source>Power %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Potenssi %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="87"/>
         <source>Base-10 Exponential %1</source>
-        <translation type="unfinished"></translation>
+        <translation>10-kantainen eksponentti %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="89"/>
         <source>Exponential %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Eksponentti %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="91"/>
         <source>Constant Pi %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Vakio pii %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="94"/>
         <source>Square Root %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Neliöjuuri %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="96"/>
         <source>Logarithm %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Logaritmi %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="98"/>
         <source>Natural Log %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Luonnollinen logaritmi %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="101"/>
         <source>Sine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Sini %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="103"/>
         <source>Cosine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Kosini %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="105"/>
         <source>Tangent %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Tangentti %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="108"/>
         <source>Arc Sine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Arkussini %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="110"/>
         <source>Arc Cosine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Arkuskosini %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="112"/>
         <source>Arc Tangent %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Arkustangentti %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="115"/>
         <source>Hyperbolic Sine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Hyperbolinen sini %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="117"/>
         <source>Hyperbolic Cosine %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Hyperbolinen kosini %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="119"/>
         <source>Hyperbolic Tangent %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Hyperbolinen tangentti %1</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="182"/>
         <source>Save Calculator History</source>
-        <translation type="unfinished"></translation>
+        <translation>Tallenna laskimen historia</translation>
     </message>
 </context>
 </TS>
