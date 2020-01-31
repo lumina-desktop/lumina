ee85f9b4254ee98a06d169d14fb3cbb37c74f098
Ken Moore <moorekou@gmail.com>
2020-01-31 15:25:37 -0500

    Tag version 1.6.0

--- src-qt5/core/libLumina/LDesktopUtils.cpp
+++ src-qt5/core/libLumina/LDesktopUtils.cpp
@@ -14,7 +14,7 @@
 #include "LuminaThemes.h"
 
 QString LDesktopUtils::LuminaDesktopVersion(){
-  QString ver = "1.5.0";
+  QString ver = "1.6.0";
   #ifdef GIT_VERSION
   ver.append( QString(" (Git Revision: %1)").arg(GIT_VERSION) );
   #endif
