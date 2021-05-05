a180baf7bdeba5686c4962999ea2cc72574cea47
Aylinux <cihanalk@gmail.com>
2021-05-05 03:16:32 +0300

    Update l-photo_tr.ts

--- src-qt5/desktop-utils/lumina-photo/i18n/l-photo_tr.ts
+++ src-qt5/desktop-utils/lumina-photo/i18n/l-photo_tr.ts
@@ -6,137 +6,138 @@
     <message>
         <location filename="../mainUI.ui" line="14"/>
         <source>Image Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Resim görüntüleyici</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="34"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Dosya</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="45"/>
         <source>&amp;View</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Göeünüm</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="57"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>Araç çubuğu</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="85"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Oynat</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="90"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>Beklet</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="95"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Dur</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="100"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>İleri</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="105"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Geri</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="110"/>
         <source>&amp;Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Çıkış</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="121"/>
         <source>&amp;Open</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Aç</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="126"/>
         <source>&amp;Save as...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Farklı kaydet...</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="131"/>
         <source>&amp;Print</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Yazdır</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="136"/>
         <source>Zoom &amp;In</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>&amp;Yakınlaştır</translatorcomment>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="141"/>
         <source>Zoom &amp;Out</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Uzaklaştır</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="146"/>
         <source>&amp;Normal Zoom</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Normal Yakınlaştırma</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="151"/>
         <source>&amp;Fit to windows</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Pencerelere sığdır</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="156"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Hakkında</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="161"/>
         <source>First</source>
-        <translation type="unfinished"></translation>
+        <translation>İlk</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="164"/>
         <source>First image</source>
-        <translation type="unfinished"></translation>
+        <translation>İlk resim</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="169"/>
         <source>Last</source>
-        <translation type="unfinished"></translation>
+        <translation>Son</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="172"/>
         <source>Last image</source>
-        <translation type="unfinished"></translation>
+        <translation>Son resim</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="113"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="174"/>
         <source>Cannot load %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 yüklenemiyor: %2</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="187"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosya Aç</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="208"/>
         <source>Save as</source>
-        <translation type="unfinished"></translation>
+        <translation>Farklı kaydet</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="220"/>
         <source>Cannot write %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 yazılamıyor :%2</translation>
     </message>
 </context>
 <context>
@@ -144,67 +145,67 @@
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="608"/>
         <source>Multimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>Multimedya</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="609"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Geliştirme</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="610"/>
         <source>Education</source>
-        <translation type="unfinished"></translation>
+        <translation>Eğitim</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="611"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>Oyunlar</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="612"/>
         <source>Graphics</source>
-        <translation type="unfinished"></translation>
+        <translation>Grafikler</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="613"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>İnternet</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="614"/>
         <source>Office</source>
-        <translation type="unfinished"></translation>
+        <translation>Ofis</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="615"/>
         <source>Science</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilim</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="616"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayarlar</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="617"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Sistem</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="618"/>
         <source>Utility</source>
-        <translation type="unfinished"></translation>
+        <translation>Araçlar</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="619"/>
         <source>Wine</source>
-        <translation type="unfinished"></translation>
+        <translation>Wine</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="620"/>
         <source>Unsorted</source>
-        <translation type="unfinished"></translation>
+        <translation>Diğer</translation>
     </message>
 </context>
 </TS>
