1503934e46e9aae88eb855acc53f34e278b55804
Aylinux <cihanalk@gmail.com>
2021-05-08 05:16:03 +0300

    Update lumina-info_tr.ts

--- src-qt5/core/lumina-info/i18n/lumina-info_tr.ts
+++ src-qt5/core/lumina-info/i18n/lumina-info_tr.ts
@@ -2,96 +2,206 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="tr">
 <context>
-    <name>MainUI</name>
+    <name>LFileDialog</name>
     <message>
-        <location filename="../MainUI.ui" line="14"/>
-        <source>Lumina Desktop Information</source>
-        <translation>Lumina Masaüstü Bilgisi</translation>
+        <location filename="../LFileDialog.ui" line="14"/>
+        <source>Open With...</source>
+        <translation>Birlikte Aç...</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="60"/>
-        <source>General</source>
-        <translation>Genel</translation>
+        <location filename="../LFileDialog.ui" line="68"/>
+        <source>Preferred</source>
+        <translation>Tercih edilen</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="89"/>
-        <source>Source Repository</source>
-        <translation>Kaynak Deposu</translation>
+        <location filename="../LFileDialog.ui" line="75"/>
+        <source>Available</source>
+        <translation>Kullanılabilir</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="124"/>
-        <source>Lumina Website</source>
-        <translation>Lumina Web Sitesi</translation>
+        <location filename="../LFileDialog.ui" line="82"/>
+        <source>Custom</source>
+        <translation>Özel</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="152"/>
-        <source>Bug Reports</source>
-        <translation>Hata Raporları</translation>
+        <location filename="../LFileDialog.ui" line="131"/>
+        <source>Binary Location</source>
+        <translation>İkili Konumu</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="179"/>
-        <source>Desktop Version:</source>
-        <translation>Masaüstü Sürümü:</translation>
+        <location filename="../LFileDialog.ui" line="138"/>
+        <source>Find</source>
+        <translation>Bul</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="202"/>
-        <source>OS Build:</source>
-        <translation>İşletim Sistemi Yapı:</translation>
+        <location filename="../LFileDialog.ui" line="199"/>
+        <source>Set this application as the default </source>
+        <translation>Bu uygulamayı varsayılan olarak belirle </translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="218"/>
-        <source>Qt Version:</source>
-        <translation>Qt Sürümü:</translation>
+        <location filename="../LFileDialog.ui" line="221"/>
+        <source>OK</source>
+        <translation>Tamam</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="276"/>
-        <source>Ask the Community</source>
-        <translation>Topluluğa Sor</translation>
+        <location filename="../LFileDialog.ui" line="235"/>
+        <source>Cancel</source>
+        <translation>İptal</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="233"/>
-        <source>View Information</source>
-        <translation>Bilgiyi Görüntüle</translation>
+        <location filename="../LFileDialog.cpp" line="40"/>
+        <source>(Email Link)</source>
+        <translation>(Eposta Bağlantısı)</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="298"/>
-        <source>License</source>
-        <translation>Lisans</translation>
+        <location filename="../LFileDialog.cpp" line="41"/>
+        <source>(Internet URL - %1)</source>
+        <translation>(İnternet URL - %1)</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="315"/>
-        <source>Acknowledgements</source>
-        <translation>Teşekkürler</translation>
+        <location filename="../LFileDialog.cpp" line="114"/>
+        <source>Audio</source>
+        <translation>Ses</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="333"/>
-        <source>Project Lead:</source>
-        <translation>Proje Yöneticisi:</translation>
+        <location filename="../LFileDialog.cpp" line="115"/>
+        <source>Video</source>
+        <translation>Video</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="340"/>
-        <source>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</source>
-        <translation>&lt;a href=&quot;https://github.com/beanpole135&quot;&gt;Ken Moore&lt;/a&gt;</translation>
+        <location filename="../LFileDialog.cpp" line="116"/>
+        <source>Multimedia</source>
+        <translation>Multimedya</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="353"/>
-        <source>Contributors:</source>
-        <translation>Katkıda bulunanlar:</translation>
+        <location filename="../LFileDialog.cpp" line="117"/>
+        <source>Development</source>
+        <translation>Geliştirme</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="366"/>
-        <source>Sponsors:</source>
-        <translation>Sponsorlar:</translation>
+        <location filename="../LFileDialog.cpp" line="118"/>
+        <source>Education</source>
+        <translation>Eğitim</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="428"/>
-        <source>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Open in web browser&lt;a&gt;</source>
-        <translation>&lt;a href=&quot;https://github.com/pcbsd/lumina/graphs/contributors&quot;&gt;Web tarayıcısında aç&lt;a&gt;</translation>
+        <location filename="../LFileDialog.cpp" line="119"/>
+        <source>Game</source>
+        <translation>Oyun</translation>
     </message>
     <message>
-        <location filename="../MainUI.ui" line="454"/>
-        <source>Close</source>
-        <translation>Kapat</translation>
+        <location filename="../LFileDialog.cpp" line="120"/>
+        <source>Graphics</source>
+        <translation>Grafik</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="121"/>
+        <source>Network</source>
+        <translation>Ağ</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="122"/>
+        <source>Office</source>
+        <translation>Ofis</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="123"/>
+        <source>Science</source>
+        <translation>Bilim</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="124"/>
+        <source>Settings</source>
+        <translation>Ayarlar</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="125"/>
+        <source>System</source>
+        <translation>Sistem</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="126"/>
+        <source>Utilities</source>
+        <translation>Gereçler</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="127"/>
+        <source>Other</source>
+        <translation>Diğer</translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="211"/>
+        <source>[default] </source>
+        <translation>[varsayılan] </translation>
+    </message>
+    <message>
+        <location filename="../LFileDialog.cpp" line="278"/>
+        <source>Find Application Binary</source>
+        <translation>Uygulama İkilisi Bul</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="54"/>
+        <source>File Error</source>
+        <translation>Dosya Hatası</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="199"/>
+        <location filename="../main.cpp" line="213"/>
+        <source>Audio Volume %1%</source>
+        <translation>Ses Düzeyi %1%</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="221"/>
+        <location filename="../main.cpp" line="230"/>
+        <source>Screen Brightness %1%</source>
+        <translation>Ekran Parlaklığı %1%</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="262"/>
+        <source>Invalid file or URL: %1</source>
+        <translation>Geçersiz Dosya ya da URL: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="283"/>
+        <source>Application entry is invalid: %1</source>
+        <translation>Başvuru girişi geçersiz:%1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="294"/>
+        <source>Application shortcut is missing the launching information (malformed shortcut): %1</source>
+        <translation>Uygulama kısayolunda başlatma bilgisi eksik (hatalı kısayol): %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="307"/>
+        <source>URL shortcut is missing the URL: %1</source>
+        <translation>URL kısayolunda URL eksik: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="318"/>
+        <source>Directory shortcut is missing the path to the directory: %1</source>
+        <translation>Dizin kısayolunda dizin yolu eksik: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="323"/>
+        <source>Unknown type of shortcut : %1</source>
+        <translation>Bilinmeyen kısayol türü: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="386"/>
+        <source>Could not find &quot;%1&quot;. Please ensure it is installed first.</source>
+        <translation>&quot;%1&quot; bulunamadı. Lütfen önce kurulduğundan emin olun.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="435"/>
+        <source>Application Error</source>
+        <translation>Uygulama Hatası</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="435"/>
+        <source>The following application experienced an error and needed to close:</source>
+        <translation>İzleyen uygulama bir hatayla karşılaştı ve kapatılması gerekti:</translation>
     </message>
 </context>
 <context>
@@ -99,67 +209,67 @@
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="608"/>
         <source>Multimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>Multimedya</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="609"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Geliştirme</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="610"/>
         <source>Education</source>
-        <translation type="unfinished"></translation>
+        <translation>Eğitim</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="611"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>Oyunlar</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="612"/>
         <source>Graphics</source>
-        <translation type="unfinished"></translation>
+        <translation>Grafik</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="613"/>
         <source>Network</source>
-        <translation type="unfinished"></translation>
+        <translation>Ağ</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="614"/>
         <source>Office</source>
-        <translation type="unfinished"></translation>
+        <translation>Ofis</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="615"/>
         <source>Science</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilim</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="616"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ayarlar</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="617"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Sistem</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="618"/>
         <source>Utility</source>
-        <translation type="unfinished"></translation>
+        <translation>Araçlar</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="619"/>
         <source>Wine</source>
-        <translation type="unfinished"></translation>
+        <translation>Wine</translation>
     </message>
     <message>
         <location filename="../../libLumina/LuminaXDG.cpp" line="620"/>
         <source>Unsorted</source>
-        <translation type="unfinished"></translation>
+        <translation>Diğer</translation>
     </message>
 </context>
 </TS>
