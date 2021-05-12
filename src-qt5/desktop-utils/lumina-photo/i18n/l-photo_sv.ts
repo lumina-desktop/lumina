a6bd95aa676df0d0fa05fbdd0baf5c65c03657f4
bittin <droidbittin@gmail.com>
2021-05-12 07:47:37 +0200

    Update l-photo_sv.ts

--- src-qt5/desktop-utils/lumina-photo/i18n/l-photo_sv.ts
+++ src-qt5/desktop-utils/lumina-photo/i18n/l-photo_sv.ts
@@ -6,137 +6,137 @@
     <message>
         <location filename="../mainUI.ui" line="14"/>
         <source>Image Viewer</source>
-        <<translation>Bildvisare></translation>
+        <translation>Bildvisare></translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="34"/>
         <source>&amp;File</source>
-        <<translation>&amp; Fil"></translation>
+        <translation>&amp; Fil</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="45"/>
         <source>&amp;View</source>
-        <<translation>&amp; Visa"></translation>
+        <translation>&amp; Visa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="57"/>
         <source>toolBar</source>
-        <<translation>verktygsfält"></translation>
+        <translation>verktygsfält</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="85"/>
         <source>Play</source>
-        <<translation>Spela"></translation>
+        <translation>Spela</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="90"/>
         <source>Pause</source>
-        <<translation>Pausa"></translation>
+        <translation>Pausa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="95"/>
         <source>Stop</source>
-        <<translation>Stoppa"></translation>
+        <translation>Stoppa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="100"/>
         <source>Next</source>
-        <<translation>Nästa"></translation>
+        <translation>Nästa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="105"/>
         <source>Back</source>
-        <<translation>Tillbaka"></translation>
+        <translation>Tillbaka</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="110"/>
         <source>&amp;Exit</source>
-        <<translation>&amp; Avsluta""></translation>
+        <translation>&amp; Avsluta"</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="121"/>
         <source>&amp;Open</source>
-        <<translation>Öppna"></translation>
+        <translation>Öppna</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="126"/>
         <source>&amp;Save as...</source>
-        <<translation>Spara som..."></translation>
+        <translation>Spara som...</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="131"/>
         <source>&amp;Print</source>
-        <<translation>&amp; Skriv ut"></translation>
+        <translation>&amp; Skriv ut</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="136"/>
         <source>Zoom &amp;In</source>
-        <<translation>Zooma &amp; in"></translation>
+        <translation>Zooma &amp; in</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="141"/>
         <source>Zoom &amp;Out</source>
-        <<translation>Zooma &amp; ut"></translation>
+        <translation>Zooma &amp; ut</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="146"/>
         <source>&amp;Normal Zoom</source>
-        <<translation>&amp; Normal zoom"></translation>
+        <translation>&amp; Normal zoom</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="151"/>
         <source>&amp;Fit to windows</source>
-        <<translation>&amp; Anpassa till fönster"></translation>
+        <translation>&amp; Anpassa till fönster</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="156"/>
         <source>&amp;About</source>
-        <<translation>&amp; Om"></translation>
+        <translation>&amp; Om</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="161"/>
         <source>First</source>
-        <<translation>Första"></translation>
+        <translation>Första</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="164"/>
         <source>First image</source>
-        <<translation>Första bilden"></translation>
+        <translation>Första bilden</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="169"/>
         <source>Last</source>
-        <<translation>Sista"></translation>
+        <translation>Sista</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="172"/>
         <source>Last image</source>
-        <<translation>Sista bilden"></translation>
+        <translation>Sista bilden</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="113"/>
         <source>Ctrl+Q</source>
-        <<translation>Ctrl+Q"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="174"/>
         <source>Cannot load %1: %2</source>
-        <<translation>Kunde inte ladda %1: %2"></translation>
+        <translation>Kunde inte ladda %1: %2</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="187"/>
         <source>Open File</source>
-        <<translation>Öppna fil"></translation>
+        <translation>Öppna fil</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="208"/>
         <source>Save as</source>
-        <<translation>Spara som"></translation>
+        <translation>Spara som</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="220"/>
         <source>Cannot write %1: %2</source>
-        <<translation>Kunde inte skriva %1: %2"></translation>
+        <translation>Kunde inte skriva %1: %2</translation>
     </message>
 </context>
 <context>
@@ -144,67 +144,67 @@
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="608"/>
         <source>Multimedia</source>
-        <<translation>Multimedia"></translation>
+        <translation>Multimedia</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="609"/>
         <source>Development</source>
-        <<translation>Utveckling"></translation>
+        <translation>Utveckling</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="610"/>
         <source>Education</source>
-        <<translation>Utbildning"></translation>
+        <translation>Utbildning</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="611"/>
         <source>Games</source>
-        <<translation>Spel"></translation>
+        <translation>Spel</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="612"/>
         <source>Graphics</source>
-        <<translation>Grafik"></translation>
+        <translation>Grafik</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="613"/>
         <source>Network</source>
-        <<translation>Nätverk"></translation>
+        <translation>Nätverk</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="614"/>
         <source>Office</source>
-        <<translation>Kontor"></translation>
+        <translation>Kontor</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="615"/>
         <source>Science</source>
-        <<translation>Vetenskap"></translation>
+        <translation>Vetenskap</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="616"/>
         <source>Settings</source>
-        <<translation>Inställningar"></translation>
+        <translation>Inställningar</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="617"/>
         <source>System</source>
-        <<translation>System"></translation>
+        <translation>System</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="618"/>
         <source>Utility</source>
-        <<translation>Verktyg"></translation>
+        <translation>Verktyg</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="619"/>
         <source>Wine</source>
-        <<translation>Wine"></translation>
+        <translation>Wine</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="620"/>
         <source>Unsorted</source>
-        <<translation>Oserterad"></translation>
+        <translation>Oserterad</translation>
     </message>
 </context>
 </TS>
