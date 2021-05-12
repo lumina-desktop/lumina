211cf71ab082763b3618251c9c3cc53f17324f7e
bittin <droidbittin@gmail.com>
2021-05-12 07:43:41 +0200

    Update l-mediap_sv.ts

--- src-qt5/desktop-utils/lumina-mediaplayer/i18n/l-mediap_sv.ts
+++ src-qt5/desktop-utils/lumina-mediaplayer/i18n/l-mediap_sv.ts
@@ -19,339 +19,339 @@
     <message>
         <location filename="../mainUI.ui" line="74"/>
         <source>(No Running Video)</source>
-        <translation>(Ingen körande video)></translation>
+        <translation>(Ingen körande video)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="105"/>
         <source>Playlist</source>
-        <translation>Spellista></translation>
+        <translation>Spellista</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="167"/>
         <source>up</source>
-        <translation>upp></translation>
+        <translation>upp</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="183"/>
         <source>down</source>
-        <translation>ner></translation>
+        <translation>ner</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="267"/>
         <source>Current Song</source>
-        <translation>Nuvarande låt></translation>
+        <translation>Nuvarande låt</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="303"/>
         <source>TITLE</source>
-        <translation>TITEL></translation>
+        <translation>TITEL</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="327"/>
         <source>ARTIST</source>
-        <translation>ARTIST></translation>
+        <translation>ARTIST</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="337"/>
         <source>ALBUM</source>
-        <translation>ALBUM></translation>
+        <translation>ALBUM</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="368"/>
         <source>Love this song</source>
-        <translation>Älska denna låt></translation>
+        <translation>Älska denna låt</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="384"/>
         <source>Tired of this song (will not play for a month)</source>
-        <translation>Trött på denna låt (kommer inte spelas på en månad)></translation>
+        <translation>Trött på denna låt (kommer inte spelas på en månad)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="397"/>
         <source>Ban this song (will never play again)</source>
-        <translation>Banna denna låt (kommer aldrig att spelas igen)></translation>
+        <translation>Banna denna låt (kommer aldrig att spelas igen)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="417"/>
         <source>View details about song (launches web browser)</source>
-        <translation>Visa detaljer om denna låt (startar webbläsare)></translation>
+        <translation>Visa detaljer om denna låt (startar webbläsare)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="448"/>
         <source>Current Station</source>
-        <translation>Nuvarande station></translation>
+        <translation>Nuvarande station</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="479"/>
         <source>Delete current station</source>
-        <translation>Ta bort nuvarande station></translation>
+        <translation>Ta bort nuvarande station</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="482"/>
         <source>rm</source>
-        <translation>=rm></translation>
+        <translation>=rm</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="492"/>
         <source>Create new station</source>
-        <translation>Skapa ny station></translation>
+        <translation>Skapa ny station</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="495"/>
         <source>add</source>
-        <translation>lägg till></translation>
+        <translation>lägg till</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="512"/>
         <source>Settings</source>
-        <translation>Inställningar></translation>
+        <translation>Inställningar</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="521"/>
         <source>Pandora Account Login</source>
-        <translation>Pandora kontoinloggning></translation>
+        <translation>Pandora kontoinloggning</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="527"/>
         <source>Email</source>
-        <translation>Email></translation>
+        <translation>Email</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="537"/>
         <source>Password</source>
-        <translation>Lösenord></translation>
+        <translation>Lösenord</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="557"/>
         <source>&lt;a href=https://www.pandora.com/account/register&gt;Need an account?&lt;/a&gt;</source>
-        <translation>&lt;a href=https://www.pandora.com/account/register&gt; behöver du ett konto?&lt;/a&gt;></translation>
+        <translation>&lt;a href=https://www.pandora.com/account/register&gt; behöver du ett konto?&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="573"/>
         <source>Audio Quality</source>
-        <translation>Ljudkvalite></translation>
+        <translation>Ljudkvalite</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="583"/>
         <source>Proxy URL</source>
-        <translation>Proxy URL></translation>
+        <translation>Proxy URL</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="593"/>
         <source>Control Proxy URL</source>
-        <translation>Kontrollera Proxy URL></translation>
+        <translation>Kontrollera Proxy URL</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="618"/>
         <source>Apply Settings</source>
-        <translation>Tillämpa inställningar></translation>
+        <translation>Tillämpa inställningar</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="656"/>
         <source>Audio Driver</source>
-        <translation>Ljuddrivrutin></translation>
+        <translation>Ljuddrivrutin</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="681"/>
         <source>&amp;File</source>
-        <translation>&amp; Fil></translation>
+        <translation>&amp; Fil</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="688"/>
         <source>&amp;View</source>
-        <translation>&amp; Visa></translation>
+        <translation>&amp; Visa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="723"/>
         <source>Play</source>
-        <translation>Spela></translation>
+        <translation>Spela</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="728"/>
         <source>Pause</source>
-        <translation>Pausa></translation>
+        <translation>Pausa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="733"/>
         <source>Stop</source>
-        <translation>Stoppa></translation>
+        <translation>Stoppa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="738"/>
         <source>Next</source>
-        <translation>Nästa></translation>
+        <translation>Nästa</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="743"/>
         <source>Back</source>
-        <translation>Förgående></translation>
+        <translation>Förgående</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="748"/>
         <source>VolUp</source>
-        <translation>Volymupp></translation>
+        <translation>Volymupp</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="751"/>
         <source>Raise audio volume</source>
-        <translation>Höj ljudvolym></translation>
+        <translation>Höj ljudvolym</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="756"/>
         <source>VolDown</source>
-        <translation>Volymner></translation>
+        <translation>Volymner</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="759"/>
         <source>Lower audio volume</source>
-        <translation>Sänk ljudvolym></translation>
+        <translation>Sänk ljudvolym</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="764"/>
         <source>Close Application</source>
-        <translation>Stäng applikation></translation>
+        <translation>Stäng applikation</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="767"/>
         <source>Ctrl+Q</source>
-        <translation>Ctrl+Q></translation>
+        <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="781"/>
         <source>Close to tray when active</source>
-        <translation>Stäng till bricka när det är aktivt></translation>
+        <translation>Stäng till bricka när det är aktivt</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="786"/>
         <source>From current artist</source>
-        <translation>Från nuvarande artist></translation>
+        <translation>Från nuvarande artist</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="789"/>
         <source>Create station from current artist</source>
-        <translation>Skapa station från nuvarande artist></translation>
+        <translation>Skapa station från nuvarande artist</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="794"/>
         <source>From current song</source>
-        <translation>Från nuvarande låt></translation>
+        <translation>Från nuvarande låt</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="797"/>
         <source>Create station from current song</source>
-        <translation>Skapa station från nuvarande låt></translation>
+        <translation>Skapa station från nuvarande låt</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="808"/>
         <source>Show song notifications</source>
-        <translation>Visa låtaviseringar></translation>
+        <translation>Visa låtaviseringar</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="813"/>
         <source>Search...</source>
-        <translation>Sök...></translation>
+        <translation>Sök...</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="816"/>
         <source>Search for a new station</source>
-        <translation>Sök efter en ny station></translation>
+        <translation>Sök efter en ny station</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="828"/>
         <location filename="../mainUI.cpp" line="276"/>
         <source>Pandora Radio</source>
-        <translation>Pandora Radio></translation>
+        <translation>Pandora Radio</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="831"/>
         <source>Stream from Pandora Radio</source>
-        <translation>Ström från Pandora Radio></translation>
+        <translation>Ström från Pandora Radio</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="839"/>
         <source>Local Files</source>
-        <translation>Lokala filer></translation>
+        <translation>Lokala filer</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="842"/>
         <source>Play Local Files</source>
-        <translation>Spela lokala filer></translation>
+        <translation>Spela lokala filer</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="105"/>
         <source>Please install the `pianobar` utility to enable this functionality</source>
-        <translation>Installera verktyget "pianobar" för att aktivera denna funktion></translation>
+        <translation>Installera verktyget "pianobar" för att aktivera denna funktion</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="109"/>
         <source>Stream music from the Pandora online radio service</source>
-        <translation>Strömma musik från Pandora online radiotjänst></translation>
+        <translation>Strömma musik från Pandora online radiotjänst</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="122"/>
         <source>Low</source>
-        <translation>Låg></translation>
+        <translation>Låg</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="123"/>
         <source>Medium</source>
-        <translation>Medel></translation>
+        <translation>Medel</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="124"/>
         <source>High</source>
-        <translation>Hög></translation>
+        <translation>Hög</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="358"/>
         <source>Open Multimedia Files</source>
-        <translation>Öppna multimediafiler></translation>
+        <translation>Öppna multimediafiler</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="406"/>
         <source>Now Playing:</source>
-        <translation>Nu spelas:></translation>
+        <translation>Nu spelas:</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="471"/>
         <source>[PLAYBACK ERROR]
 %1</source>
-        <translation>[UPPSPELNINGS FEL]></translation>
+        <translation>[UPPSPELNINGS FEL]</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="488"/>
         <source>Media Loading...</source>
-        <translation>Media laddar...></translation>
+        <translation>Media laddar...</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="490"/>
         <source>Media Stalled...</source>
-        <translation>Media stannade...></translation>
+        <translation>Media stannade...</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="492"/>
         <source>Media Buffering...</source>
-        <translation>Media buffrar...></translation>
+        <translation>Media buffrar...</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="576"/>
         <source>Pandora: Create Station</source>
-        <translation>Pandora: Skapa en station></translation>
+        <translation>Pandora: Skapa en station</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="576"/>
         <source>Search Term</source>
-        <translation>Sökterm></translation>
+        <translation>Sökterm</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="654"/>
         <source>Pandora Question</source>
-        <translation>Pandora fråga></translation>
+        <translation>Pandora fråga</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="659"/>
         <source>Pandora Error</source>
-        <translation>Pandora fel></translation>
+        <translation>Pandora fel</translation>
     </message>
 </context>
 <context>
@@ -359,7 +359,7 @@
     <message>
         <location filename="../PianoBarProcess.cpp" line="358"/>
         <source>Could not find any matches. Please try a different search term</source>
-        <translation type=Det gick inte att hitta några matchningar. Försök med en annan sökterm></translation>
+        <translation type=Det gick inte att hitta några matchningar. Försök med en annan sökterm</translation>
     </message>
 </context>
 <context>
@@ -367,67 +367,67 @@
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="608"/>
         <source>Multimedia</source>
-        <translation>Multimedia></translation>
+        <translation>Multimedia</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="609"/>
         <source>Development</source>
-        <translation>Utveckling></translation>
+        <translation>Utveckling</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="610"/>
         <source>Education</source>
-        <translation<Utbildning></translation>
+        <translation<Utbildning</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="611"/>
         <source>Games</source>
-        <translation>Spel></translation>
+        <translation>Spel</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="612"/>
         <source>Graphics</source>
-        <translation type=Grafik></translation>
+        <translation type=Grafik</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="613"/>
         <source>Network</source>
-        <translation>Nätverk></translation>
+        <translation>Nätverk</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="614"/>
         <source>Office</source>
-        <translation>Kontor></translation>
+        <translation>Kontor</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="615"/>
         <source>Science</source>
-        <translation>Vetenskap></translation>
+        <translation>Vetenskap</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="616"/>
         <source>Settings</source>
-        <translation>Inställningar></translation>
+        <translation>Inställningar</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="617"/>
         <source>System</source>
-        <translation>System></translation>
+        <translation>System</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="618"/>
         <source>Utility</source>
-        <translation>Verktyg></translation>
+        <translation>Verktyg</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="619"/>
         <source>Wine</source>
-        <translation>Wine></translation>
+        <translation>Wine</translation>
     </message>
     <message>
         <location filename="../../../core/libLumina/LuminaXDG.cpp" line="620"/>
         <source>Unsorted</source>
-        <translation>Oserterat></translation>
+        <translation>Oserterat</translation>
     </message>
 </context>
 </TS>
