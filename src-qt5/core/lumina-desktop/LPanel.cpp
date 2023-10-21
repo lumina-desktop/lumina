29a961bf118f30ea5d1cf13f1afa10877048102a
Jason W. Bacon <bacon4000@gmail.com>
2023-10-21 16:42:15 -0500

    Uncomment a window resize handler, so the screen redraws properly when resized.  Important when running under VirtualBox.

--- src-qt5/core/lumina-desktop/LPanel.cpp
+++ src-qt5/core/lumina-desktop/LPanel.cpp
@@ -74,7 +74,10 @@ LPanel::LPanel(QSettings *file, QString scr, int num, QWidget *parent, bool rese
     //panelArea->setWindowOpacity(1.0); //fully opaque for the widget on top (apply stylesheet transparencies)
   }
   QTimer::singleShot(1,this, SLOT(UpdatePanel()) );
-  //connect(screen, SIGNAL(resized(int)), this, SLOT(UpdatePanel()) ); //in case the screen resolution changes
+  // This apparently should not be necessary, but the main panel does not
+  // redraw reliably without it.  Be sure to fully test resizing a
+  // FreeBSD VirtualBox guest before replacing this.
+  connect(screen, SIGNAL(resized(int)), this, SLOT(UpdatePanel()) ); //in case the screen resolution changes
 }
 
 LPanel::~LPanel(){
